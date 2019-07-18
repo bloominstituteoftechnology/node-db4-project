fadb894b51ee20cead305d5908a37d080e76027c
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-19 01:15:03 +0200

    added a welcome message to the root api endpoint

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const RecipeRouter = require('./recipes/recipe-router.js');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send(`<h2>Welcome to the API of Sprint 12 Lecture 4 Challenge</h2>`);
+});
+
 server.use(express.json());
 server.use('/api/recipes', RecipeRouter);
 
