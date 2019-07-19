a0551579199e1c609005b949a53c78ed8dc37ffd
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-19 05:11:43 +0200

    refactored the knex seeds to be more future proof for deletions

--- data/seeds/002-recipes.js
+++ data/seeds/002-recipes.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('recipes')
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex('recipes').insert([
--- data/seeds/005-steps.js
+++ data/seeds/005-steps.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('steps')
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex('steps').insert([
