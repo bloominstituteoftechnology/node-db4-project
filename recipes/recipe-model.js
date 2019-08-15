009347f5aaae1e129bddce04ced4e76c0ba5230d
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-15 16:10:17 -0400

    Add Recipe model

---
+++ recipes/recipe-model.js
@@ -0,0 +1,15 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+}
+
+function getRecipes() {
+  return db('recipes');
+};
+
+function getShoppingList(recipe_id) {};
+
+function getInstructions(recipe_id) {};
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-const db = require('../data/db-config.js');
+const Recipes = require('./recipe-model.js');
 
 // GET api/recipes/
 router.get('/', async (req, res) => {
-    const recipes = await db('recipes');
-
+    const recipes = await Recipes.getRecipes();
     res.status(200).json(recipes);
 });
 
