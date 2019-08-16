fd2d9d19ad83fbc85df75f63c2809786d24e17cd
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-15 22:17:28 -0400

    Return shopping list

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -10,6 +10,23 @@ function getRecipes() {
   return db('recipes');
 };
 
-function getShoppingList(recipe_id) {};
+// returns a list of all ingredients and quantities for a given recipe
+function getShoppingList(recipe_id) {
+  const id = recipe_id;
+
+//   select r.name, i.name, ri.quantity from recipes as r
+//   join recipe_ingredients as ri on r.id = ri.recipe_id
+//   join ingredients as i on i.id = ri.ingredient_id
+//   where r.id = 1
+
+  return db('recipes as r')
+    .join('recipe_ingredients as ri', 'r.id', 'ri.recipe_id')
+    .join('ingredients as i', 'i.id', 'ri.ingredient_id')
+    .select('r.name', 'i.name', 'ri.quantity')
+    .where({"r.id": id})
+};
+
+
+
 
 function getInstructions(recipe_id) {};
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -8,4 +8,19 @@ router.get('/', async (req, res) => {
     res.status(200).json(recipes);
 });
 
+// GET /api/recipes/:id/shoppingList
+router.get('/:id/shoppingList', async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const shoppingList = await Recipes.getShoppingList(id);
+    console.log(shoppingList);
+
+    res.status(200).json(shoppingList);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({message: 'Cannot get recipe'});
+  }
+})
+
 module.exports = router;
