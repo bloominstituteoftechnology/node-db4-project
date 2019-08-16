61dbb75ac16e1bfba931bfd6aaaeeb863cab9d8a
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-15 23:26:39 -0400

    Error management for shopping list

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -19,7 +19,11 @@ router.get('/:id/shoppingList', async (req, res) => {
   try {
     const shoppingList = await Recipes.getShoppingList(id);
 
-    res.status(200).json(shoppingList);
+    if (shoppingList && shoppingList.length > 0) {
+      res.status(200).json(shoppingList);
+    } else {
+      res.status(404).json({message: "Cannot find that shopping list"});
+    }
   } catch (err) {
     res.status(500).json({message: "Cannot get shopping list"});
   }
