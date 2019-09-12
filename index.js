64e8c664656faa2a019a2e3b06d5ccdc441a9023
johnschneider1 <johnlawschneider@gmail.com>
2019-09-12 15:02:07 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require("./server.js");
+
+const port = 4575;
+
+server.listen(port, () => console.log(`\n Check out my API ON  ${port} **\n`));
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "host"
+    )}`
+  );
+  next();
+}
+
+module.exports = server;
