a6358f3000c9d569ce58f974c3938f1f11df52cb
dAVE Inden <davidinden@gmail.com>
2019-11-07 15:26:33 -0800

    config db and setup helper functions

---
+++ data/db-recipe-helper.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+
+
+function getRecipes() {
+
+};
+
+function getShoppingList(recipe_id) {
+
+};
+
+function getInstructions(recipe_id) {
+
+};
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+};
