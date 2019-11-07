b3d541e304451c2bf96224b0ad341f45d7969378
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-07 17:11:56 -0600

    finish mvp

--- helpers/recipes-model.js
+++ helpers/recipes-model.js
@@ -3,6 +3,7 @@ const db = require('../data/dbconfig.js')
 module.exports = {
     getRecipes,
     getShoppingList,
+    getInstructions
 }
 
 function getRecipes(){
@@ -10,10 +11,17 @@ function getRecipes(){
 }
 
 function getShoppingList(recipe_id){
-    return('recipe_ingredients as ri')
+    return db('recipe_ingredients as ri')
         .select('ingredients.name', 'ri.qunatity')
         .where({ recipe_id })
         .join('ingredients', 'ingredients.id', 'ri.ingredient_id')
 
+}
+
+function getInstructions(recipe_id){
+    return db('steps as s')
+        .select('recipes.name', 's.instruction')
+        .where({ recipe_id })
+        .join('recipes', 'recipes.id', 'steps.recipe_id')
 }
 
