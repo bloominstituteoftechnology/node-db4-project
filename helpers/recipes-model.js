6a21f5672c704b12cc862bc0209f4d879ace7370
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-07 17:08:39 -0600

    added another helper function

--- helpers/recipes-model.js
+++ helpers/recipes-model.js
@@ -9,3 +9,11 @@ function getRecipes(){
     return db('recipes')
 }
 
+function getShoppingList(recipe_id){
+    return('recipe_ingredients as ri')
+        .select('ingredients.name', 'ri.qunatity')
+        .where({ recipe_id })
+        .join('ingredients', 'ingredients.id', 'ri.ingredient_id')
+
+}
+
