0059b3e429011a882859a4b784a253081c80b510
seongck <seongk@pm.me>
2019-11-21 20:08:29 -0800

    Add create-tables migration

--- data/migrations/20191121195343_create-tables.js
+++ data/migrations/20191121195343_create-tables.js
@@ -1,8 +1,54 @@
 
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+      .createTable('recipes', tbl => {
+        tbl.increments('recipe_id');
+
+        tbl.string('recipe_name', 255).notNullable()
+      })
+      .createTable('ingredients', tbl => {
+        tbl.increments('ingredient_id');
+
+        tbl.string('ingredient_name', 255).notNullable();
+      })
+      .createTable('instructions', tbl => {
+        tbl.increments('instructions_id');
+
+        tbl.integer('recipe_id')
+          .unsigned()
+          .notNullable()
+          .references('recipes.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+      })
+      .createTable('recipes_ingredients', tbl => {
+        tbl.integer('recipe_id')
+          .unsigned()
+          .notNullable()
+          .references('recipes.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+
+        tbl.integer('ingredient_id')
+          .unsigned()
+          .notNullable()
+          .references('ingredients.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+
+        tbl.string('unit', 255)
+          .notNullable();
+
+        tbl.integer('ingredient_quantity')
+          .notNullable();
+
+        tbl.primary(['recipe_id', 'ingredient_id']);
+      })
+  )
 };
 
 exports.down = function(knex) {
+  
   
 };
