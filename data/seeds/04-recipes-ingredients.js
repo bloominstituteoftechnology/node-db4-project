56386178ff05b359e2e6e32fbd8cdfd8174309d3
seongck <seongk@pm.me>
2019-11-21 20:57:11 -0800

    Add 04-recipes-ingredients seeds

---
+++ data/seeds/04-recipes-ingredients.js
@@ -0,0 +1,34 @@
+exports.seed = function(knex) {
+  return knex('recipes_ingredients').insert([
+    {
+      recipe_id: 1,
+      ingredient_id: 1,
+      unit: "Slice",
+      ingredient_quantity: 2
+    },
+    {
+      recipe_id: 1,
+      ingredient_id: 2,
+      unit: "Slice",
+      ingredient_quantity: 2
+    },
+    {
+      recipe_id: 1,
+      ingredient_id: 3,
+      unit: "TBSP",
+      ingredient_quantity: 2
+    },
+    {
+      recipe_id: 2,
+      ingredient_id: 4,
+      unit: "Slice",
+      ingredient_quantity: 6
+    },
+    {
+      recipe_id: 2,
+      ingredient_id: 5,
+      unit: "Egg",
+      ingredient_quantity: 3
+    }
+  ]);
+};
