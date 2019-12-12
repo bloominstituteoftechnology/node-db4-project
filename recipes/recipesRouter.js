0a7acfd10f76e1dc805f085ed472d6e7fc2ed3b8
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-12 14:58:52 -0800

    made router file (incomplete), completed server.js - should be at MVP but haven't tested functions yet

---
+++ recipes/recipesRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const Recipes = require('../data/db-model')
+
+const router = express.Router()
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 
 //will require the router(s)
+const recipesRouter = require('./recipes/recipesRouter')
 
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
+
+server.use('/api/recipes', recipesRouter)
 
 module.exports = server;
