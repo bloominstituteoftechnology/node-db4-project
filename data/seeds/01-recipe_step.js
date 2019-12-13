892a59cf254ca9c4226ac014dfd1f0801bfe58e8
Anthony Hill <hillan1152@gmail.com>
2019-12-12 23:38:38 -0500

    created seeds

--- data/seeds/01-recipe_step.js
+++ data/seeds/01-recipe_step.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipe_step')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('recipe_step').insert([
+        {quantity: 2, unit_of_measurement: "Teaspoons", ingredient_id: 1}
       ]);
     });
 };
--- data/seeds/02-instructions.js
+++ data/seeds/02-instructions.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('instructions')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('instructions').insert([
+        { recipe_step_id: 1, instruction: "1. Toast the bagel and spread CC" }
       ]);
     });
 };
