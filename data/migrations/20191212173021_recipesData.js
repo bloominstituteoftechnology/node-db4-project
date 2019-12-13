810af338225f13d4c928c71d93efe26d5ef8ed9f
Anthony Hill <hillan1152@gmail.com>
2019-12-12 23:28:24 -0500

    seeds created

--- data/migrations/20191212173021_recipesData.js
+++ data/migrations/20191212173021_recipesData.js
@@ -5,8 +5,40 @@ exports.up = function(knex) {
 
       tbl.string('ingredient_name', 255).notNullable();
   })
+  .createTable('recipe_step', tbl => {
+      tbl.increments();
+
+      tbl.string('unit_of_measurement', 255).notNullable();
+
+      tbl.float('quantity').notNullable();
+
+      tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredient')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  })
+  .createTable('instructions', tbl => {
+      tbl.increments();
+
+      tbl.string('instruction', 255).notNullable();
+
+      tbl.integer('recipe_step_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredient')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema    
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('recipe_step')
+    .dropTableIfExists('ingredients');
 };
