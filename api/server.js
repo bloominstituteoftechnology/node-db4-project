a4b290bb3359cb3b1a069ef045b85757ff4f65b9
Brianna Workman <brianna.workman@hotmail.com>
2019-12-12 19:10:03 -0600

    Sets up file structure and migration tables

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const recipeRouter = require("../recipes/recipe-router.js");
+const ingredientRouter = require("../recipes/ingredient-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/recipes", recipeRouter);
+server.use("/api/ingredients", ingredientRouter);
+
+module.exports = server;
---
+++ data/migrations/20191212181351_recipeTables.js
@@ -0,0 +1,42 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl.string("name", 255).notNullable();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl.string("name", 255).notNullable();
+    })
+    .createTable("instructions", tbl => {
+      tbl.increments();
+      tbl.string("step", 255).notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes");
+    })
+    .createTable("recipe_ingredients", tbl => {
+      tbl.primary(["recipe_id", "ingredient_id"]);
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {};
