1c3ed6c5d5d4a25ef94c971592c14c3129e688cf
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-23 14:59:37 +0000

    migrations created

--- migrations/20200123142310_new-recipe-book.js
+++ migrations/20200123142310_new-recipe-book.js
@@ -1,8 +1,47 @@
+exports.up = function (knex) {
+    return (knex.schema
+        .createTable('recipes', table => {
+            table.increments();
+            table.string('name', 128);
+            notNullable();
+        })
+        .createTable('ingredients', table => {
+            table.increments();
+            table.string('name', 128);
+        })
+        .createTable('steps', table => {
+            table.increments();
+            table.integer('step_number')
+                .notNullable()
+                .unsigned();
+            table.integer('recipe_id')
+                .notNullable()
+                .unsigned()
+                .references('recipes.id');
+            table.text('Please add instructions', 128)
+        })
+        .createTable('recipe_ingredients', table => {
+            table.increments();
+            table.integer('recipe_id')
+                .notNullable()
+                .unsigned()
+                .references('recipes.id');
+            table.integer('ingredient_id')
+                .notNullable()
+                .unsigned()
+                .references('ingredients.id');
+            table.float('ammount');
+        })
 
-exports.up = function(knex) {
-  
+    )
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return (knex.schema
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipe_ingrdients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('recipes')
+
+    )
 };
