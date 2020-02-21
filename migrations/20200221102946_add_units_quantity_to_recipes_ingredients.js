61a8d5bdb27853a9c9b803898ff14514ff58dbec
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-21 10:32:58 -0500

    completed migrations

---
+++ migrations/20200221102946_add_units_quantity_to_recipes_ingredients.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.table("recipes_ingredients", tbl => {
+    tbl.string("units", 255);
+    tbl.float("quantity");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("recipes_ingredients", clmn => {
+    clmn.dropColumn("quantity");
+    clmn.dropColumn("units");
+  });
+};
