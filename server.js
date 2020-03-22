237662130aec78478b10feaeed9fc0d9cd60e269
mastaryan <sizemore.ryan@gmail.com>
2020-03-22 18:35:23 -0400

    knex working and server has started

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const RecipeRouter = require('./recipes/recipe-router');
+const server = express();
+
+server.use(express.json());
+server.use('/api/recipes', RecipeRouter);
+
+module.exports = server;
