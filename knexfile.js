af5bfb7ce7a1b2d7a0500553c9c57753d22352f3
jackskim <jackskim29@gmail.com>
2020-04-14 20:26:50 -0700

    Rename recipes.db3 to recipebook.db3

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/recipes.db3'
+      filename: './data/recipebook.db3'
     },
     migrations: {
       directory: './data/migrations'
