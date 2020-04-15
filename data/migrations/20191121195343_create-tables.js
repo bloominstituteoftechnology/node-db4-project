3084e3c2e2390dd457998e5d2bace2777da1a361
jackskim <jackskim29@gmail.com>
2020-04-14 20:31:24 -0700

    Change ingredient quantity to floats

--- data/migrations/20191121195343_create-tables.js
+++ data/migrations/20191121195343_create-tables.js
@@ -1,64 +1,59 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
 
-exports.up = function(knex) {
-  return (
-    knex.schema
-      .createTable('recipes', tbl => {
-        tbl.increments('recipe_id');
+      tbl.string("recipe_name", 255).notNullable();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
 
-        tbl.string('recipe_name', 255).notNullable();
-      })
-      .createTable('ingredients', tbl => {
-        tbl.increments('ingredient_id');
+      tbl.string("ingredient_name", 255).notNullable().unique();
+    })
+    .createTable("instructions", (tbl) => {
+      tbl.increments("instructions_id");
 
-        tbl.string('ingredient_name', 255).notNullable().unique();
-      })
-      .createTable('instructions', tbl => {
-        tbl.increments('instructions_id');
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipes.recipe_id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
 
-        tbl.integer('recipe_id')
-          .unsigned()
-          .notNullable()
-          .references('recipes.recipe_id')
-          .onDelete('CASCADE')
-          .onUpdate('CASCADE');
+      tbl.text("step").notNullable();
 
-        tbl.text('step').notNullable();
+      tbl.integer("step_number").notNullable();
+    })
+    .createTable("recipes_ingredients", (tbl) => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipes.recipe_id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
 
-        tbl.integer('step_number').notNullable();
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredients.ingredient_id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
 
-      })
-      .createTable('recipes_ingredients', tbl => {
-        tbl.integer('recipe_id')
-          .unsigned()
-          .notNullable()
-          .references('recipes.recipe_id')
-          .onDelete('CASCADE')
-          .onUpdate('CASCADE');
+      tbl.string("unit", 255).notNullable();
 
-        tbl.integer('ingredient_id')
-          .unsigned()
-          .notNullable()
-          .references('ingredients.ingredient_id')
-          .onDelete('CASCADE')
-          .onUpdate('CASCADE');
+      tbl.float("ingredient_quantity").notNullable();
 
-        tbl.string('unit', 255)
-          .notNullable();
-
-        tbl.integer('ingredient_quantity')
-          .notNullable();
-
-        tbl.primary(['recipe_id', 'ingredient_id']);
-      })
-  )
+      tbl.primary(["recipe_id", "ingredient_id"]);
+    });
 };
 
-exports.down = function(knex) {
-  return (
-    knex.schema
-      .dropTableIfExists('recipes_ingredients')
-      .dropTableIfExists('instructions')
-      .dropTableIfExists('ingredients')
-      .dropTableIfExists('recipes')
-  )
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipes_ingredients")
+    .dropTableIfExists("instructions")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };
--- data/seeds/04-recipes-ingredients.js
+++ data/seeds/04-recipes-ingredients.js
@@ -1,34 +1,34 @@
-exports.seed = function(knex) {
-  return knex('recipes_ingredients').insert([
+exports.seed = function (knex) {
+  return knex("recipes_ingredients").insert([
     {
       recipe_id: 1,
       ingredient_id: 1,
       unit: "Slice",
-      ingredient_quantity: 2
+      ingredient_quantity: 2.0,
     },
     {
       recipe_id: 1,
       ingredient_id: 2,
       unit: "Slice",
-      ingredient_quantity: 2
+      ingredient_quantity: 2.0,
     },
     {
       recipe_id: 1,
       ingredient_id: 3,
       unit: "TBSP",
-      ingredient_quantity: 2
+      ingredient_quantity: 2.0,
     },
     {
       recipe_id: 2,
       ingredient_id: 4,
       unit: "Slice",
-      ingredient_quantity: 6
+      ingredient_quantity: 6.0,
     },
     {
       recipe_id: 2,
       ingredient_id: 5,
       unit: "Egg",
-      ingredient_quantity: 3
-    }
+      ingredient_quantity: 3.0,
+    },
   ]);
 };
