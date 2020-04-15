b8ba102c6a21aef4bc795002fc3d39b3fe7fca24
jackskim <jackskim29@gmail.com>
2020-04-14 22:42:27 -0700

    Add put and delete endpoints and handlers for recipes, and add POST and DELETE for instructions

--- routers/recipes-router.js
+++ routers/recipes-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Recipes = require("../models/recipes-model.js");
+const Instructions = require("../models/instructions-model");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
@@ -32,6 +33,39 @@ router.get("/:id", validateRecipeId, async (req, res, next) => {
   }
 });
 
+router.put(
+  "/:id",
+  validateRecipeId,
+  validateRecipeBody,
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const result = await Recipes.update(id, req.body);
+      if (+result === 1) {
+        const updatedRecipe = await Recipes.getById(id);
+        res.json(updatedRecipe);
+      } else {
+        next({ message: "Failed to update recipe in database" });
+      }
+    } catch (error) {
+      next({ message: "Failed to update recipe" });
+    }
+  }
+);
+
+router.delete("/:id", validateRecipeId, async (req, res, next) => {
+  try {
+    const result = await Recipes.remove(req.params.id);
+    if (+result === 1) {
+      res.status(204);
+    } else {
+      next({ message: "Failed to delete recipe" });
+    }
+  } catch (error) {
+    next({ message: "Failed to delete recipe" });
+  }
+});
+
 router.get("/:id/shoppinglist", validateRecipeId, (req, res, next) => {
   const { id } = req.params;
 
@@ -52,42 +86,47 @@ router.get("/:id/instructions", validateRecipeId, (req, res, next) => {
       res.json(instructions);
     })
     .catch((err) => {
-      next({ message: "Failed to retrieve instructions" });
+      next({ message: "Failed to retrieve instructions from database" });
     });
 });
 
-router.put(
-  "/:id",
+router.post(
+  "/:id/instructions",
   validateRecipeId,
-  validateRecipeBody,
+  validateInstruction,
   async (req, res, next) => {
     const { id } = req.params;
+    const instruction = req.body;
+    instruction.recipe_id = id;
+    instruction.step_number = +instruction.step_number;
     try {
-      const result = await Recipes.update(id, req.body);
-      if (+result === 1) {
-        const updatedRecipe = await Recipes.getById(id);
-        res.json(updatedRecipe);
-      } else {
-        next({ message: "Failed to update recipe" });
-      }
+      const [instructionId] = await Instructions.add(instruction);
+      const newInstruction = await Instructions.getById(instructionId);
+      res.status(201).json(newInstruction);
     } catch (error) {
-      next({ message: "Failed to update recipe" });
+      next({ message: "Failed to add recipe step" });
     }
   }
 );
 
-router.delete("/:id", validateRecipeId, async (req, res, next) => {
-  try {
-    const result = await Recipes.remove(req.params.id);
-    if (+result === 1) {
-      res.status(204);
-    } else {
-      next({ message: "Failed to delete recipe" });
+router.delete(
+  "/:id/instructions/:instructionId",
+  validateRecipeId,
+  validateInstructionId,
+  async (req, res, next) => {
+    try {
+      const result = await Instructions.remove(req.params.instructionId);
+
+      if (+result === 1) {
+        res.status(204);
+      } else {
+        next({ message: "Failed to delete recipe step from database " });
+      }
+    } catch (error) {
+      next({ message: "Failed to delete recipe step" });
     }
-  } catch (error) {
-    next({ message: "Failed to delete recipe" });
   }
-});
+);
 
 function validateRecipeBody(req, res, next) {
   const recipe = req.body;
@@ -109,6 +148,30 @@ async function validateRecipeId(req, res, next) {
         })
       : next();
   } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+function validateInstruction(req, res, next) {
+  const instruction = req.body;
+  if (!instruction.step || !instruction.step_number) {
+    next({ status: 400, message: "Please provide a step number and step" });
+  } else {
+    next();
+  }
+}
+
+async function validateInstructionId(req, res, next) {
+  try {
+    const instruction = await Instructions.getById(req.params.instructionId);
+
+    !instruction.length
+      ? next({
+          status: 404,
+          message: "Instruction does not exist",
+        })
+      : next();
+  } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
