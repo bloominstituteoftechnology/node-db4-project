2f8ffc8342037616a274da172227e8c46d24db10
jackskim <jackskim29@gmail.com>
2020-04-14 21:12:14 -0700

    Update primary key names

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,8 +1,7 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('recipes').insert([
-    {recipe_id: 1, recipe_name: 'Grilled Cheese Sandwich'},
-    {recipe_id: 2, recipe_name: 'Bacon & Eggs'}
+  return knex("recipes").insert([
+    { id: 1, recipe_name: "Grilled Cheese Sandwich" },
+    { id: 2, recipe_name: "Bacon & Eggs" },
   ]);
 };
--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,11 +1,10 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('ingredients').insert([
-    {ingredient_id: 1, ingredient_name: 'Bread'},
-    {ingredient_id: 2, ingredient_name: 'Cheese'},
-    {ingredient_id: 3, ingredient_name: 'Butter'},
-    {ingredient_id: 4, ingredient_name: 'Bacon'},
-    {ingredient_id: 5, ingredient_name: 'Eggs'}
+  return knex("ingredients").insert([
+    { id: 1, ingredient_name: "Bread" },
+    { id: 2, ingredient_name: "Cheese" },
+    { id: 3, ingredient_name: "Butter" },
+    { id: 4, ingredient_name: "Bacon" },
+    { id: 5, ingredient_name: "Eggs" },
   ]);
 };
-
