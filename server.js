69f418610f187f888bbfd15f915b2d774e0a559a
jackskim <jackskim29@gmail.com>
2020-04-14 20:35:22 -0700

    Add cors and root error handler

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
-
-const helmet = require('helmet');
-const RecipeRouter = require('./recipes/recipes-router.js');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const RecipeRouter = require("./routers/recipes-router.js");
 
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
-server.use('/api/recipes', RecipeRouter);
-//server.use('/api/ingredients', IngredientsRouter);
+
+server.use("/api/recipes", RecipeRouter);
+//server.use("/api/ingredients", IngredientsRouter);
+
+server.use(errorHandler);
+
+function errorHandler(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+}
 
 module.exports = server;
