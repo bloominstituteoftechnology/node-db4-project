b689259750c7e06984f6c8a96f748c2f94c1cd8f
Kristi Gribble <kgribble4083@gmail.com>
2020-04-17 12:58:16 -0500

    doesn't return ingredients except in the console.

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -2,6 +2,7 @@ const db = require("../data/db-config.js");
 
 module.exports = {
     getRecipe,
+    getShoppingList,
     // addRecipe,
     // findRecipeById,
     // updateRecipe,
@@ -11,3 +12,15 @@ module.exports = {
 function getRecipe() {
     return db("recipes")
 }
+
+function getShoppingList(recipe_id) {
+    return db("ingredients")
+    .join("recipes", "recipes.id", "ingredients.recipe_id")
+    .select("ingredients.ingredients", "ingredients.quantity")
+    .where({ recipe_id })
+
+}
+
+// SELECT recipes.title, ingredients.quantity, ingredients.ingredients FROM ingredients
+// JOIN recipes
+// WHERE recipes.id = ingredients.recipe_id
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -14,4 +14,23 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id/ingredients', (req, res) => {
+    const { id } = req.params
+
+    Recipes.getShoppingList(id)
+        .then(ingredients => {
+            console.log(ingredients)
+            if (ingredients) {
+                req.status(200).json(ingredients)
+            } else {
+                req.status(404).json({ message: "No ingredients were found" })
+            }
+        })
+        
+        .catch(err => {
+            res.status(500).json({ message: "Could not retrieve ingredients list" })
+        })
+    
+})
+
 module.exports = router;
