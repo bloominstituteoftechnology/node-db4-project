0c470a9d38358b1f2de9438662397718d90e8d5f
Kristi Gribble <kgribble4083@gmail.com>
2020-04-16 22:37:07 -0500

    Fixed bridge table, changed amt from int to dec to capture floating point numbers

--- data/migrations/20200416212849_create-table.js
+++ data/migrations/20200416212849_create-table.js
@@ -3,16 +3,21 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("recipes", table => {
         table.increments()
-        table.string("title", 128).notNullable()
+        table.string("title", 128)
+            .notNullable()
     })
 
     .createTable("ingredients", table => {
         table.increments()
-        table.string("ingredients", 128).notNullable()
+        table.decimal("quantity", null)
+            .notNullable()
+        table.string("ingredients", 128)
+            .notNullable()
         table
             .integer("recipe_id")
             .unsigned()
             .notNullable()
+            .references("id")
             .inTable("recipes")
             .onUpdate("CASCADE")
             .onDelete("CASCADE");
@@ -20,19 +25,42 @@ exports.up = function(knex) {
 
     .createTable("instructions", table => {
         table.increments()
-        table.text("instructions", 255).notNullable()
+        table.integer("step_number")
+            .unsigned()
+            .notNullable()
+        table.text("instructions", 255)
+            .notNullable()
         table
             .integer("recipe_id")
             .unsigned()
             .notNullable()
+            .references("id")
             .inTable("recipes")
             .onUpdate("CASCADE")
             .onDelete("CASCADE");
+    })
+
+    .createTable("recipes_ingredients", table => {
+        table.increments()
+        table.integer("recipe_id")
+            .unsigned()
+            .references("id")
+            .inTable("recipes")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        table.integer("ingredients_id")
+            .unsigned()
+            .references("id")
+            .inTable("ingredients")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+
     })
 };
 
 exports.down = function(knex) {
     return knex.schema
+    .dropTableIfExists("recipes_ingredients")
     .dropTableIfExists("instructions")
     .dropTableIfExists("ingredients")
     .dropTableIfExists("recipes")
