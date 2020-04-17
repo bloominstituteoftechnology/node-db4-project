f650041d191d654112252f2e008fc578a90269da
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-16 21:47:34 -0400

    added in the seeds folder and specifed helper methods

--- post-router/router-model.js
+++ post-router/router-model.js
@@ -26,10 +26,41 @@ function remove(id) {
         .del();
 }
 
+function getRecipes() {
+    return db('recipes')
+  }
+
+  function getRecipeIngredients(id) {
+    return db
+      .select('recipes.name')
+      .from('recipes')
+      .join('recipe_ingredients', 'recipes.id', 'recipe_ingredients.recipeId')
+      .join('ingredients', 'ingredients.id', 'recipe_ingredients.ingredientId')
+      .where('ingredients.id', '=', id)
+  }
+  
+function getShoppingList(recipeId) {
+    return db('recipe_ingredients')
+        .join('ingredients', 'recipe_ingredients.ingredientId', 'ingredients.id')
+        .select('ingredients.name', 'recipe_ingredients.quantity', 'recipe_ingredients.unit')
+        .where('recipe_ingredients.recipeId', '=', recipeId)
+}
+
+function getInstructions(recipeId) {
+    return db
+    .select('step_number', 'instruction')
+    .from('instructions')
+    .where({recipeId})
+  }
+
 module.exports = {
     find,
     findById,
     add,
     update,
-    remove
+    remove,
+    getRecipes,
+    getRecipeIngredients,
+    getShoppingList,
+    getInstructions
 }
--- post-router/router.js
+++ post-router/router.js
@@ -65,7 +65,82 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params;
 
+    Recipes.remove(id)
+    .then(deleted => {
+        if (!deleted) {
+            res.status(400).json({error: 'Could not delete item with given id'})
+        } else {
+            res.status(201).json({message: 'Deleted the recipe'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Falied to delete the recipe' })
+    })
 })
+
+
+router.get('/', (req, res) => {
+    Recipes.getRecipe()
+    .then(recipes =>{
+        if (!recipes){
+            res.status(404).json({error: 'Recipe list not found'})
+        } else {
+        res.json(201).json({message: 'Rendering Recipe List...', recipes})
+    }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'Failed to find the recipe list', err})
+    })      
+})
+
+router.get('/:id/recipe', (req, res) => {
+    const {id} = req.params;
+
+    Recipes.getRecipeIngredients()
+        .then(ingredient => {
+            if (!ingredient) {
+                res.status(404).json({ error: 'Could not find the ingredients for the recipe with set id, please try again'})
+            } else {
+                res.status(201).json({ message: 'Ingredients needed: ', ingredient })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Could not find the ingredients for the recipe', err})
+        })
+})
+
+router.get('/:id/shoppingList', (req, res) => {
+    const {id} = req.params
+
+    Recipes.getShoppingList(id)
+        .then(list => {
+            if (!list) {
+                res.status(404).json({error: 'Could not find the shopping list with specified id'})
+            } else {
+                res.status(201).json({list})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Could not find the shopping list', err})
+        })
+})
+
+router.get('/:id/instructions', (req, res) => {
+    const {id} = req.params
+    Recipes.getInstructions(id)
+        .then(instruction =>{
+            if (!instruction) {
+                res.status(404).json({error: 'Could not find the instructions with specified id'})
+            } else {
+                res.status(201).json({instruction})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Could not find the instructions', err})
+        })
+})
+
 
 module.exports = router;
