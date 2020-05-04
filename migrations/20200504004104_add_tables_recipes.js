dc7dd0d7e359ccf9750fd03c4139d19f1a91aa99
Michael Lowe <mrlowememphis@yahoo.com>
2020-05-04 00:50:38 -0500

    migrations updated

--- migrations/20200504004104_add_tables_recipes.js
+++ migrations/20200504004104_add_tables_recipes.js
@@ -1,8 +1,68 @@
-
 exports.up = function(knex) {
-  
-};
 
-exports.down = function(knex) {
+    return knex.schema
+      .createTable("recipes", tbl => {
+        tbl.increments();
   
-};
+        tbl
+          .string("title", 128)
+          .notNullable()
+          .unique();
+  
+        tbl.string("description", 255).notNullable();
+      })
+      .createTable("ingredients", tbl => {
+        tbl.increments();
+  
+        tbl
+          .string("name", 255)
+          .notNullable()
+          .unique();
+      })
+      .createTable("steps", tbl => {
+        tbl.increments();
+  
+        tbl
+          .integer("step_number")
+          .unsigned()
+          .notNullable();
+  
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .references("id")
+          .inTable("recipes")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+      .createTable("recipes_ingredients", tbl => {
+        tbl.primary(["recipe_id", "ingredient_id"]);
+  
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("recipes")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+  
+        tbl
+          .integer("ingredient_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("ingredients")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      });
+  };
+  
+  exports.down = function(knex) {
+  
+    return knex.schema
+      .dropTableIfExists("recipes_ingredients")
+      .dropTableIfExists("steps")
+      .dropTableIfExists("ingredients")
+      .dropTableIfExists("recipes");
+  };
