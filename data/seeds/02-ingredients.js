e6503253ea87f3b268978570faff23bd4da97eda
Chris Harwell <chrisharwell72@gmail.com>
2020-08-31 16:32:35 -0400

    MVP Complete

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('ingredients').insert([
-        {id: 1, ingedient: 'pickle', quantity: 2.4},
-        {id: 2, ingedient: 'sausage', quantity: 4.7},
-        {id: 3, ingedient: 'bacon', quantity: 3.6},
+        {id: 1, ingredient: 'pickle', quantity: 2.4},
+        {id: 2, ingredient: 'sausage', quantity: 4.7},
+        {id: 3, ingredient: 'bacon', quantity: 3.6},
       ]);
     });
 };
--- data/seeds/03-recipes.js
+++ data/seeds/03-recipes.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('recipe').insert([
-        {id: 1, name: 'fried chicken'},
+        {id: 1, instructionId: 1, ingredientId: 1, name: 'fried chicken'},
         // {id: 2, instructionId: 2, ingredientId: 2, name: 'piza'},
         // {id: 3, instructionId: 3, ingredientId: 3, name: 'corn dogs'}
       ]);
