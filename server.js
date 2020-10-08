33aa614da32166de746d1a2b5aa85b2e21bda1a4
victor dronov <victordronov1@gmail.com>
2020-10-08 13:31:59 -0400

    set up

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
+});
+
+module.exports = server
