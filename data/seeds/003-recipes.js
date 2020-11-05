ed5c5a5a5fd0ef98e1977c831b40a0d0826ca0b6
willwearing <willwearing@gmail.com>
2020-11-05 15:16:10 -0700

    mvp done

--- data/seeds/003-recipes.js
+++ data/seeds/003-recipes.js
@@ -1,8 +1,8 @@
 exports.seed = function (knex, Promise) {
   return knex("recipes").insert([
-    { recipe_title: "yams", recipe_book_id: 1 },
-    { recipe_title: "test1", recipe_book_id: 1 },
-    { recipe_title: "test2", recipe_book_id: 1 },
-    { recipe_title: "test3", recipe_book_id: 1 },
+    { recipe_title: "yams", instructions: "cook the yams", recipe_book_id: 1 },
+    { recipe_title: "test1", instructions: "test 1", recipe_book_id: 1 },
+    { recipe_title: "test2", instructions: "test 2", recipe_book_id: 1 },
+    { recipe_title: "test3", instructions: "test 3", recipe_book_id: 1 },
   ]);
 };
