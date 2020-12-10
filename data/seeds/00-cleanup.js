4f6083fc5aa6a05068087d1d38f9a9cb7999fb75
Ramsha <ramshanasir.rn@gmail.com>
2020-12-10 17:37:41 -0600

    MVP

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,11 +1,3 @@
-// const cleaner = require('knex-cleaner');
-
-// exports.seed = function(knex) {
-//   return cleaner.clean(knex, {
-//     mode: 'truncate', // resets ids
-//     ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
-//   });
-// };
 
 exports.seed = async function(knex) {
 	await knex("steps & ingredients").truncate()
--- data/seeds/03-steps.js
+++ data/seeds/03-steps.js
@@ -1,12 +1,4 @@
-// exports.seed = function(knex, Promise) {
-//     return knex('steps').insert([
-//         { step_number: 1, recipe_id:1 },
-//         { step_number: 2 , recipe_id:5},
-//         { step_number: 3, recipe_id:6 },
-//         { step_number: 4, recipe_id:4},
-//         { step_number: 5, recipe_id:3}
-//     ]);
-// };
+
 exports.seed = async function(knex){
     await knex("steps").insert([
       {step_number: 1, instruction: "Roast Turkey at 375f", recipe_id: 1},
