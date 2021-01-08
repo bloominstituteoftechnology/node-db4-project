9e5d47bc80dfd64bb19fe2c85b9242f6c7bc691e
Terry Edwards <terryedwardsjr113@gmail.com>
2021-01-07 22:02:23 -0500

    created-tables

---
+++ knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/recipeBook.db3'
+    },
+    useNullAsDefault:true
+  },
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+
+  // this is needed when using foreign keys
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
+    },
+  }
+}
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
