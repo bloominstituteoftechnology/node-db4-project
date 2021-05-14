89178e1ac6304ad9d9ed26f3ed4c6e5a256d58ea
Daniel Brannon <daniel-brannon@hotmail.com>
2021-05-14 02:21:44 -0500

    wip

--- data/migrations/20210514070440_initial-migration.js
+++ data/migrations/20210514070440_initial-migration.js
@@ -2,16 +2,36 @@
 exports.up = async function(knex) {
   await knex.schema
   .createTable('recipes', table => {
-      table.increments()
+      table.increments('recipe_id')
+      table.string('recipe_name', 200).notNullable().unique()
   })
   .createTable('ingredients', table => {
-      table.increments()
+      table.increments('ingredient_id')
+      table.string('ingredient_name', 200).notNullable().unique()
+      table.string('ingredient_unit', 50)
   })
   .createTable('steps', table => {
-      table.increments()
+      table.increments('step_id')
+      table.string('step_text', 200).notNullable()
+      table.string('step_number').notNullable()
+      table.interger('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('recipe_id')
+      .inTable('recipes')
+      .onDelete('RESTRICT')
+      .onUpdate('RESTRICT')
   })
   .createTable('step_ingredients', table => {
-      table.increments()
+      table.increments('step_ingredient_id')
+      table.float('quantity').notNullable()
+      table.interger('step_id')
+      .unsigned()
+      .notNullable()
+      .references('recipe_id')
+      .inTable('recipes')
+      .onDelete('RESTRICT')
+      .onUpdate('RESTRICT')
   })
 };
 
