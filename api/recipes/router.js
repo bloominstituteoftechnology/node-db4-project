250104efe2d30d14cd1abadda003c596c29298ee
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-10 16:29:36 -0600

    router set up

--- api/recipes/router.js
+++ api/recipes/router.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const { checkRecipeId } = require('./middleware')
 
 const router = express.Router()
+
+router.get('/:id', checkRecipeId, (req, res) => {
+    res.status(200).json(req.recipe)
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.message || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const router = require('./recipes/router')
 
 const server = express()
 
 server.use(express.json())
-
+server.use('/api/recipes', router)
 module.exports = server
