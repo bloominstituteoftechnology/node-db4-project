75cf04a3cc18cbb0d4694e9817971b59f9b7ea41
Kaseem Bradley <kaseembradley@Meryls-phone.localdomain>
2021-06-10 15:20:11 -0400

    completed data schema

--- data/migrations/20210610183836_first-table.js
+++ data/migrations/20210610183836_first-table.js
@@ -1,7 +1,49 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("recipes", (tbl) => {
-    tbl.increments("recipe_id");
-  });
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable().unique();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredient_name", 128).unique().notNullable();
+      tbl.integer("ingredient_quantity").notNullable();
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.string("step_text", 128).notNullable();
+      tbl
+        .integer("ingredient_id")
+        .notNullable()
+        .unsigned()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT");
+    })
+    .createTable("recipe_options", (tbl) => {
+      tbl.increments("option_id");
+      tbl
+        .integer("recipe_id")
+        .notNullable()
+        .unsigned()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT");
+
+      tbl
+        .integer("ingredient_id")
+        .notNullable()
+        .unsigned()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT");
+    });
 };
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_options")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
