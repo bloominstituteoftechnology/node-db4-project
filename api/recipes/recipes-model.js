cd70e6a9113441712ee8146690d9a6aff9ebe5d8
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-11 00:04:21 -0400

    Almost finished

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,12 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe ${recipe_id}`)
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+        .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+        .leftJoin('step')
+        .where('recipe_id', recipe_id)
+    
+        return recipeRows
 }
 
 module.exports = { getRecipeById }
