4b553d2f7e0eddc196813bb69f5b4df01ec1429f
yonathan Admasu <admyoni4@yahoo.com>
2021-06-24 23:36:43 -0700

    finally finished

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,20 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe with ${recipe_id}`)
+const db = require('..//../data/db-confin')
+
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+      .leftJoin('steps as s', 'r.recipe_id','s.recipe_id')
+      .leftJoin('step_ingredients as si', 'si.step_id', 's.step_id')
+      .select(
+          'r.recipe_id',
+          'r.recipe_name',
+          's.step_id',
+          's.step_number',
+          's.step_intructions',
+          'si.ingredient_id',
+      )
+      .where('r.recipe_id', recipe_id)
+      return recipeRows
 }
 
 module.exports = { getRecipeById }
