c6e5b53c3627bb7d75dbbf840e161f9b1354febd
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-15 11:14:13 -0500

    created initial server scaffolding with required installs

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('*', (req, res, next) => {
+    next({status: 404,
+        message: 'not found'
+    })
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server;
