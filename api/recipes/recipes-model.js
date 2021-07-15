e7a532ab2c34bfe3a930a9cdf073b07ec3e32c0e
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-15 16:02:54 -0500

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -6,9 +6,25 @@ async function getRecipes() {
 }
 
 async function getRecipeById(recipe_id) {
-    const recipeById = await db('recipes as r')
+    const recipe = await db('recipes as r')
+        // .select('r.recipe_id', 'r.recipe_name', 'r.created_at')
+        // .where('r.recipe_id', recipe_id).first()
+        .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
         .where('r.recipe_id', recipe_id)
-    return recipeById
+        .select('r.*', 's.step_id', 's.step_number', 's.step_instruction')
+        .orderBy('s.step_number')
+    return recipe
+    
+    // const result = {
+    //     recipe_id: recipe[0].recipe_id,
+    //     recipe_name: recipe[0].recipe_name,
+    //     created_at: recipe[0].created_at,
+    //     steps: []
+    // }
+
+    // recipe.forEach() => {
+        
+    // });
 }
 
 module.exports = {
