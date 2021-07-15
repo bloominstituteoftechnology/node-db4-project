1f8e772ce41cda3bffed8d5ebb0d6c74f647c07b
Michael-Kochis <rhishisikk@gmail.com>
2021-07-15 14:30:11 -0500

    steps attaching to recipe

--- api/recipes/recipe-router.js
+++ api/recipes/recipe-router.js
@@ -1,18 +1,19 @@
 const express = require('express')
-const {  } = require('./recipe-middleware')
+const { checkRecipeId } = require('./recipe-middleware')
 const Recipes = require('./recipe-model.js')
 
 const router = express.Router()
 
-router.get('/:recipe_id', checkSchemeId, (req, res, next) => {
+router.get('/:recipe_id', checkRecipeId, (req, res, next) => {
     const { recipe_id } = req.params
   
-    Recipes.findById(recipe_id)
-      .then(recipe => {
-        res.json(recipe)
-      })
-      .catch(next)
-  })
+    returnThis = req.recipe;
+    res.status(200).json(returnThis);
+
+    if (returnThis.id === -1) {
+      next();
+    }
+});
   
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
