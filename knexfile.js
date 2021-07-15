82e6c4e57305d51d712ea5efa1dc8944ae8d0642
wzd200 <william.z.dye@gmail.com>
2021-07-15 16:52:21 -0400

    Made small change to db location

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ const sharedConfig = {
   module.exports = {
     development: {
       ...sharedConfig,
-      connection: { filename: './data/schemes.db3' },
+      connection: { filename: './data/recipes.db3' },
     },
     testing: {
       ...sharedConfig,
