89359523d156cab3c104287a4f0edc22faa4971b
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-15 16:32:31 -0500

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -10,11 +10,17 @@ async function getRecipeById(recipe_id) {
         // .select('r.recipe_id', 'r.recipe_name', 'r.created_at')
         // .where('r.recipe_id', recipe_id).first()
         .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+        .leftJoin('step_ingredients as si', 's.step_id', 'si.step_id')
+        .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
         .where('r.recipe_id', recipe_id)
-        .select('r.*', 's.step_id', 's.step_number', 's.step_instruction')
+        .select('r.*', 's.step_id', 's.step_number', 's.step_instruction', 'i.ingredient_id', 'i.ingredient_name', 'si.quantity')
         .orderBy('s.step_number')
     return recipe
     
+    // const ingredients = await db('ingredients as i')
+    //     .leftJoin('step_ingredients as si', 'i.ingredient_id', 'st.ingredient_id')
+    //     .where('')
+
     // const result = {
     //     recipe_id: recipe[0].recipe_id,
     //     recipe_name: recipe[0].recipe_name,
