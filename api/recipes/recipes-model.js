cdeb6af9f1822282d32ef24763c1ad889093bf2b
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-15 17:24:43 -0500

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -6,31 +6,47 @@ async function getRecipes() {
 }
 
 async function getRecipeById(recipe_id) {
-    const recipe = await db('recipes as r')
-        // .select('r.recipe_id', 'r.recipe_name', 'r.created_at')
-        // .where('r.recipe_id', recipe_id).first()
+    const recipes = await db('recipes as r')
         .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
         .leftJoin('step_ingredients as si', 's.step_id', 'si.step_id')
         .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
-        .where('r.recipe_id', recipe_id)
-        .select('r.*', 's.step_id', 's.step_number', 's.step_instruction', 'i.ingredient_id', 'i.ingredient_name', 'si.quantity')
+        .select(
+            'r.*', 
+            's.step_id', 
+            's.step_number', 
+            's.step_instruction', 
+            'i.ingredient_id', 
+            'i.ingredient_name', 
+            'si.quantity',
+            )
         .orderBy('s.step_number')
-    return recipe
+        .where('r.recipe_id', recipe_id)
     
-    // const ingredients = await db('ingredients as i')
-    //     .leftJoin('step_ingredients as si', 'i.ingredient_id', 'st.ingredient_id')
-    //     .where('')
+    const result = {
+        recipe_id: recipes[0].recipe_id,
+        recipe_name: recipes[0].recipe_name,
+        created_at: recipes[0].created_at,
+        steps: []
+    }
 
-    // const result = {
-    //     recipe_id: recipe[0].recipe_id,
-    //     recipe_name: recipe[0].recipe_name,
-    //     created_at: recipe[0].created_at,
-    //     steps: []
-    // }
-
-    // recipe.forEach() => {
-        
-    // });
+    recipes.forEach(recipe => {
+        if(recipe.step_id) {
+            result.steps.push({
+                step_id: recipe.step_id,
+                step_number: recipe.step_number,
+                step_instructions: recipe.step_instruction,
+                ingredients: []
+            })
+            if(recipe.ingredient_id) {
+                result.steps[0].ingredients.push({
+                    ingredient_id: recipe.ingredient_id,
+                    ingredient_name: recipe.ingredient_name,
+                    quantity: recipe.quantity
+                })
+            }
+        }
+    });
+    return result
 }
 
 module.exports = {
