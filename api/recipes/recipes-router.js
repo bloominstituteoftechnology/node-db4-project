18d15bfef92a134f6a3cb8bff993c14e883fdcbd
wzd200 <william.z.dye@gmail.com>
2021-07-15 16:23:07 -0400

    Added error-handling middleware

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -21,4 +21,12 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      sageAdvice: 'Work for 20 min, take a break',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
 module.exports = router
