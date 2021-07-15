947e5c5640c4a0ae3dcf854b77f0d4b18f648fe4
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-15 12:52:11 -0500

    wip

--- migrations/20210715164821_first-migration.js
+++ migrations/20210715164821_first-migration.js
@@ -2,39 +2,58 @@
 exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', table => {
-            table.increments('recipes_id')
+            table.increments('recipe_id')
             table.string('recipe_name', 128)
                 .unique()
                 .notNullable()
 
         })
         .createTable('steps', table => {
-            table.increments('steps_id')
+            table.increments('step_id')
             table.integer('step_number')
                 .unsigned()
                 .notNullable()
-
-        })
-        .createTable('instructions', table => {
-            table.increments('instructions_id')
             table.text('instruction_name')
-                .unique()
                 .notNullable()
-
         })
+        // .createTable('instructions', table => {
+        //     table.increments('instruction_id')
+        //     table.text('instruction_name')
+        //         .unique()
+        //         .notNullable()
+
+        // })
         .createTable('ingredients', table => {
-            table.increments('ingredients_id')
+            table.increments('ingredient_id')
             table.string('ingredient_name', 128)
                 .unique()
                 .notNullable()
 
         })
+        .createTable('recipe_ingredients', table => {
+            table.increments('recipe_ingredients_id')
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references("recipe_id")
+                .inTable("recipes")
+                .onDelete("RESTRICT")
+                .onUpdate("RESTRICT")            
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references("ingredient_id")
+                .inTable("ingredients")
+                .onDelete("RESTRICT")
+                .onUpdate("RESTRICT")    
+        })
 };
 
 exports.down = async function(knex) {
     await knex.schema
+        .dropTableIfExists('recipe_ingredients')
         .dropTableIfExists('ingredients')
-        .dropTableIfExists('instructions')
+        // .dropTableIfExists('instructions')
         .dropTableIfExists('steps')
         .dropTableIfExists('recipes')
 };
