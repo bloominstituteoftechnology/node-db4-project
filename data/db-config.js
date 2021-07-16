49aa61806e6a4fd5db7e2bcfda59fea189e579a1
AdrianHDev <adrianhensondev@gmail.com>
2021-07-16 00:25:22 -0400

    Revert "MVP Met"
    This reverts commit 82456f17542d6555ad191e6cf41e6f6c008d0a3c.

--- data/db-config.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex');
-
-const config = require('../knexfile')
-const kenv = process.env.NODE_ENV || 'development'
-
-
-module.exports = knex(config[kenv]);
--- index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./api/server');
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log("Listening on:", port);
-});
