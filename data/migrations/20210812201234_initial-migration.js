010814272b2b54a883c61476166ada59ad816b0a
Alex Edwards <alextcedwards@gmail.com>
2021-08-12 20:45:55 -0600

    wip

--- data/migrations/20210812201234_initial-migration.js
+++ data/migrations/20210812201234_initial-migration.js
@@ -1,37 +1,46 @@
 exports.up = async function (knex) {
   await knex.schema
-    .createTable("recipes", tbl => {
-        tbl.increments('recipe_id')
-        tbl.string('recipe_name', 200).notNullable().unique()
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 200).notNullable().unique();
     })
-    .createTable("ingredients", tbl => {
-        tbl.increments('ingredient_id')
-        tbl.string('ingredients_name', 200).notNullable().unique()
-        tbl.string('ingredient unit', 50)
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredients_name", 200).notNullable().unique();
+      tbl.string("ingredient unit", 50);
     })
-    .createTable("steps", tbl => {
-        tbl.increments('step_id')
-        tbl.string('step_text', 200).notNullable()
-        tbl.integer('step_number').notNullable()
-        tbl.integer('recipe_id')
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.string("step_text", 200).notNullable();
+      tbl.integer("step_number").notNullable();
+      tbl
+        .integer("recipe_id")
         .unsigned()
         .notNullable()
-        .references('recipe_id')
-        .inTable('recipes')
-        .onDelete('RESTRICT')
-        .onUpdate('RESTRICT')
-
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
-    .createTable("step_ingredients", table => {
-        table.increments('step_ingredient_id')
-        table.float('quantity').notNullable()
-        table.integer('step_id')
+    .createTable("step_ingredients", (tbl) => {
+      tbl.increments("step_ingredient_id");
+      tbl.float("quantity").notNullable();
+      tbl
+        .integer("step_id")
         .unsigned()
         .notNullable()
-        .references('step_id')
-        .inTable('steps')
-        .onDelete('RESTRICT')
-        .onUpdate('RESTRICT')
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredients_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
