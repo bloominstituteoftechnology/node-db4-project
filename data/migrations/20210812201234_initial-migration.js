86c86bb04589e033d98d5a6a802086b1528b0da0
Alex Edwards <alextcedwards@gmail.com>
2021-08-12 20:35:42 -0600

    wip

--- data/migrations/20210812201234_initial-migration.js
+++ data/migrations/20210812201234_initial-migration.js
@@ -23,7 +23,15 @@ exports.up = async function (knex) {
 
     })
     .createTable("step_ingredients", table => {
-        table.increments()
+        table.increments('step_ingredient_id')
+        table.float('quantity').notNullable()
+        table.integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     });
 };
 
