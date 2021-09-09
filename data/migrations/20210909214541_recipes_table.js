ce6535bca3692ab7a5cd056bab756d90eaaa10d1
tdubs <contact-tdubs@protonmail.com>
2021-09-09 15:17:40 -0700

    fixed typo

--- data/migrations/20210909214541_recipes_table.js
+++ data/migrations/20210909214541_recipes_table.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('recipes', table => {
-    table.increments('recipes_id')
+    table.increments('recipe_id')
     table.string('recipe_name')
       .notNullable()
     table.timestamps(true, true)
