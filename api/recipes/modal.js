0963ece4992482d9790e8caf1c025657b7b1a169
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-09 19:34:21 -0400

    added braffords code

--- api/recipes/modal.js
+++ api/recipes/modal.js
@@ -1,15 +1,35 @@
 const db = require("../../data/db-config")
 
-const findRecipeById = id => {
-    return db("recipes as r")
-        .leftJoin("steps as s", "s.recipe_id", "r.recipe_id")
-        .leftJoin("step_ingredients as si", "si.step_id", "si.step_id")
-        .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id")
-        .where("r.recipe_id", id)
-        .select("r.*", "s.step_instructions", 
-        "s.step_id", "s.step_number",
-        "si.igredient_id", "si.quantity",
-        "i.ingredient_name")
-        .orderBy("s.step_number")
+
+const findRecipeById = async id => {
+    const response = await db("recipes as r")
+        .leftJoin('steps as st', 'r.recipe_id', 'st.recipe_id')
+        .leftJoin('step_ingredients as si', 'st.step_id', 'si.step_id')
+        .leftJoin('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
+        .select('r.recipe_name', 'r.recipe_id','r.created_at', 'st.*', 'si.*', 'i.*')
+        .where('r.recipe_id', id)
+
+    if (!response.length) return false
+    // return response
+    return {
+        recipe_id: id,
+        recipe_name: response[0].recipe_name,
+        created_at: response[0].created_at,
+        steps: response[0].step_id
+            ? response.map(res => ({
+                step_id: res.step_id,
+                step_number: res.step_number,
+                step_instructions: res.step_instructions,
+            }))
+            : [],
+        ingredients: response[0].ingredient_id
+            ? response.map(res => ({
+                ingredient_id: res.ingredient_id,
+                ingredient_name: res.ingredient_name,
+                quantity: res.quantity
+            })) 
+            : []
+    }
 }
+
 module.exports = findRecipeById
--- api/server.js
+++ api/server.js
@@ -5,4 +5,12 @@ const recipeEndpoints = require("./recipes/endpoints")
 server.use(express.json())
 server.use("/api/recipes", recipeEndpoints)
 
+server.use((err, req, res, next) => {
+    const { status, message, stack } = err
+    res.status(status || 500).json({
+        message,
+        stack
+    })
+})
+
 module.exports = server
