10e61e876a7ba883b0f8c4425fee3c4eee0e0ef5
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-09 17:34:05 -0400

    created knex file

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+      connection: {
+        filename: "./data/recipes.db3",
+      },
+    },
+  },
+
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+      connection: {
+        filename: "./data/testing.db3",
+      },
+    },
+  },
+};
