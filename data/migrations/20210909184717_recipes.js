f66ab14c07058f1d5ef55c726a884b1354a2c1d4
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-09 15:40:39 -0400

    basic template handled

--- data/migrations/20210909184717_recipes.js
+++ data/migrations/20210909184717_recipes.js
@@ -1,18 +1,41 @@
 
 exports.up = async function(knex) {
-  const { createTable } = knex.schema
-    await createTable("recipes")
-            .increments("recipe_id")
-        .createTable("ingredients")
-            .increments("ingredient_id")
-        .createTable("steps")
-            .increments("step_id")
-
+    await knex.schema
+        .createTable("recipes", table => {
+            table.increments("recipe_id")
+            table.string("recipe_name")
+                .notNullable()
+            table.datetime("created_at")
+        }).createTable("ingredients", table => {
+            table.increments("ingredient_id")
+            table.string("ingredient_name")
+                .notNullable()
+        }).createTable("steps", table => {
+            table.increments("step_id")
+            table.integer("recipe_id")
+                .references("recipe_id")
+                .inTable("recipes")
+            table.integer("step_number")
+                .notNullable()
+            table.string("step_instructions")
+                .notNullable()
+        }).createTable("step_ingredients", table => {
+            table.increments("step_ingredient_id")
+            table.integer("step_id")
+                .references("step_id")
+                .inTable("steps")
+            table.integer("ingredient_id")
+                .references("ingredient_id")
+                .inTable("ingredients")
+            table.decimal("quantity", null)
+                .notNullable()
+        })
 };
 
 exports.down = async function(knex) {
-  const { dropTableIfExists } = knex.schema
-    await dropTableIfExists("recipes")
-        .dropTableIfExists("ingredients")
+    await knex.schema
+        .dropTableIfExists("step_ingredients")
         .dropTableIfExists("steps")
+        .dropTableIfExists("ingredients")
+        .dropTableIfExists("recipes")
 };
