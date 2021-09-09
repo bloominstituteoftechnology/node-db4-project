9b9af0b62ff6804544a5fe71d9bd1f466dcb1b2a
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-09 19:17:25 -0400

    set up router

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const express = require("express");
+const server = require("./api/server.js");
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("listening on 5000");
+});
