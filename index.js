1ac5323ece915c9d0b1820a87564bb5d253cb362
dominthebox <dsallustro@gmail.com>
2021-09-09 20:38:38 -0400

    wip, wired up server.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./api/server')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 server.listen(PORT, () => {
     console.log(`\nAPI running on PORT: ${PORT}\n`)
