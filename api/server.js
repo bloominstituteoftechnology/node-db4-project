2ced50bb8809079151eef35f6828aaf909cb16a9
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-10 07:02:06 -0400

    "fixed down"

--- api/server.js
+++ api/server.js
@@ -8,10 +8,4 @@ server.use("*", (req, res) => {
     res.json({api: "up"})
 })
 
-// server.use((err, req, res,next) => {
-//     res.status(err.status || 500).json({
-//         message: err.message,
-//     })
-// })
-
 module.exports = server;
