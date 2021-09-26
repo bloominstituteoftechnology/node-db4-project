95d72e91b4a18aab14c0bc6bd064dd350508b5a4
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-27 00:35:38 +0200

    wip server index router model

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
 
-router.use('*', (req, res) => {
-    res.json( {api: 'up'})
+const Recipe = require('./recipes-model')
+
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            //throw new Error('error example')
+            res.status(200).json(resource)
+        })
+        .catch(next)
 })
 
 router.use( (err, req, res, next) => { //eslint-disable-line
