b65e661ba01bb25adeb73260d97db6837578429b
Rick <ricksmycodeguy@gmail.com>
2021-10-07 17:55:57 -0500

    built out index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config({path: 'path-to-.env'})
 const server = require("./api/server")
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT;//see .env
 
 server.listen(port, ()=> 
     console.log(`\nAPI is running on port ${port}\n`)
-)
+);
