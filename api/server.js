a23147c03dcdd1514c03fcd40b93f7595cea7795
Klove Adams <kloveadams@gmail.com>
2021-10-07 17:02:16 -0400

    cleanup & set up for debugger

--- api/server.js
+++ api/server.js
@@ -8,11 +8,11 @@ server.use(express.json());
 
 server.use("/api/recipes", recipesRouter);
 
-server.use("*", (req, res, next) => {
+server.use("*", (req, res) => {
   res.json("inside server");
 });
 
-server.use((err, req, res, next) => {
+server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
   });
