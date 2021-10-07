5908a57a1aec249f658fc6ff4de76632202e3cff
nhamilton1 <nickh3592@gmail.com>
2021-10-07 17:57:35 -0400

    getting somewhere with it looking like the sample data

--- api/model.js
+++ api/model.js
@@ -4,11 +4,39 @@ const getRecipes = () => {
     return db('recipes')
 }
 
-const getRecipeById = async (id) => {
-    const rows = await db('recipies as r')
+const getRecipeById = async (recipe_id) => {
+    const rows = await db('recipes as r')
         .leftJoin('steps as st', 'st.recipe_id', 'r.recipe_id')
-        .leftJoin('step_ingredients as si', 'st.step_number', 'si.step_number')
+        .leftJoin('steps_ingredients as si', 'st.step_id', 'si.step_id')
         .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
+        .select(
+            'r.recipe_id', 
+            'r.recipe_name',
+            'r.created_at', 
+            'st.step_id', 
+            'st.step_number',
+            'st.step_instructions',
+            'i.ingredient_id',
+            'i.ingredient_name',
+            'si.quantity_oz',
+        )
+        .orderBy("st.step_number")
+        .where('r.recipe_id', recipe_id)
+
+    const recipe = {
+        recipe_id: rows[0].recipe_id,
+        recipe_name: rows[0].recipe_name,
+        steps: rows[0].step_id
+            ? rows.map(row => ({
+                step_id: row.step_id,
+                step_number: row.step_number,
+                step_instructions: row.step_instructions,
+                ingredients: []
+            }))
+            : []
+    }
+
+    return recipe
 }
 
 module.exports = {
--- api/router.js
+++ api/router.js
@@ -12,9 +12,7 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/:id', (req, res, next) => {
-    const { recipe_id } = req.params
-
-    Recipe.getRecipeById(recipe_id)
+    Recipe.getRecipeById(req.params.id)
         .then(recipeById => {
             res.json(recipeById)
         })
