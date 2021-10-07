41a9bf1a0b2139a12a4251b0f1c5f73634781a79
Christina Yun <poncheeyun@yahoo.com>
2021-10-07 15:42:48 -0700

    modeled out the endpoints and wrote out the error handling

---
+++ api/recipes/recipe-router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const { } = require('./recipe-middleware');
+const Recipes = require('./recipe-model');
+
+const router = express.Router();
+
+//[GET] /api/recipes
+router.get('/', (req, res, next) => {})
+
+//[GET] /api/recipes/:id
+router.get('/:id', (req, res, next) => {})
+
+
+//error handling
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = router;
