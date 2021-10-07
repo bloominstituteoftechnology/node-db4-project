6d50a5a785fb00f9ab3c2e732dc77ed7f199e7a7
nhamilton1 <nickh3592@gmail.com>
2021-10-07 16:50:11 -0400

    made new branch

--- data/migrations/20211007181426_first-migration.js
+++ data/migrations/20211007181426_first-migration.js
@@ -1,47 +1,48 @@
 
 exports.up = async function (knex) {
-    await knex.schema.createTable('recipes', table => {
-        table.increments('recipe_id')
-        table.string('recipe_name', 128).notNullable().unique()
+    await knex.schema
+    .createTable("recipes", table => {
+        table.increments("recipe_id");
+        table.string("recipe_name", 128).unique().notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now())
     })
-
-    await knex.schema.createTable('steps', table => {
-        table.increments('step_id')
-        table.integer('step_number').notNullable()
-        table.string('step_instructions').notNullable()
-        table.integer('recipe_id')
-            .unsigned()
-            .notNullable()
-            .references('recipe_id')
-            .inTable('recipe_id')
-            .onUpdate('RESTRICT')
-            .onDelete('RESTRICT')
+    
+    .createTable("steps", table => {
+    table.increments("step_id");
+    table.integer("step_number").unsigned().notNullable();
+    table.string("step_instructions", 256).notNullable();
+    table.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    
+    .createTable("ingredients", table => {
+    table.increments("ingredient_id");
+    table.string("ingredient_name", 128).unique().notNullable();
+    })
+    
+    .createTable("steps_ingredients", table => {
+    table.increments("steps_ingredients_id");
+    table.integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    table.integer("ingredient_id")
+        .unsigned()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    table.integer("quantity_oz").unsigned();
     })
 
-    await knex.schema.createTable('ingredients', table => {
-        table.increments('ingredient_id')
-        table.string('ingredient_name').notNullable()
-    })
-
-    await knex.schema.createTable('steps_ingredients', table => {
-        table.increments('steps_ingredients_id')
-        table.integer('step_id')
-            .unsigned()
-            .notNullable()
-            .references('step_id')
-            .inTable('steps')
-            .onUpdate('RESTRICT')
-            .onDelete('RESTRICT')
-        table.integer('ingredient_id')
-            .unsigned()
-            .notNullable()
-            .references('ingredient_id')
-            .inTable('ingredients')
-            .onUpdate('RESTRICT')
-            .onDelete('RESTRICT')
-        table.decimal('quantity_oz').notNullable()
-    })
 };
 
 exports.down = async function (knex) {
