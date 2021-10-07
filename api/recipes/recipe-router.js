7870387c00dacbbf11ef686fa69c95654e9daa17
Christina Yun <poncheeyun@yahoo.com>
2021-10-07 15:51:19 -0700

    Building out the get by id endpoint

--- api/recipes/recipe-router.js
+++ api/recipes/recipe-router.js
@@ -4,8 +4,8 @@ const Recipes = require('./recipe-model');
 
 const router = express.Router();
 
-//[GET] /api/recipes
-router.get('/', (req, res, next) => {})
+// //[GET] /api/recipes
+// router.get('/', (req, res, next) => {})
 
 //[GET] /api/recipes/:id
 router.get('/:id', (req, res, next) => {})
