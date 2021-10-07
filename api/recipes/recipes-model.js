273dd42c4ceee801b55db8e2a3622fe99ea117a2
Klove Adams <kloveadams@gmail.com>
2021-10-07 19:32:38 -0400

    almost done get still needs to return ingredients

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,9 +2,28 @@ const db = require("../../data/db-config");
 
 async function getById(recipe_id){
   const recipe = await db("recipes as r")
-  .where("recipe_id", recipe_id);
+    .join("steps as s", "r.recipe_id", "s.recipe_id")
+    .where("r.recipe_id", recipe_id)
+    .select("r.recipe_name", "s.*")
+    .orderBy("s.step_number");
 
-  return recipe
+  const result = {
+    recipe_id: recipe[0].recipe_id,
+    recipe_name: recipe[0].recipe_name,
+    steps: [],
+  };
+
+  recipe.forEach((row) => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        step_instructions: row.step_instructions,
+      });
+    }
+  })
+
+  return result;
 }
 
 module.exports = { getById };
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -3,10 +3,10 @@ const Recipe = require("./recipes-model");
 
 router.get("/:recipe_id", (req, res, next) => {
   Recipe.getById(req.params.recipe_id)
-  .then(stuff => {
-    res.status(200).json(stuff)
-  })
-  .catch(next)
+    .then((recipe) => {
+      res.status(200).json(recipe);
+    })
+    .catch(next);
 });
 
 module.exports = router;
