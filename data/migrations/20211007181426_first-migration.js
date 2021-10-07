2e7eb81035c423d182ae7cc4ebc2ab3bde3c4365
nhamilton1 <nickh3592@gmail.com>
2021-10-07 15:49:42 -0400

    fixed knexfile, finished migration

--- data/migrations/20211007181426_first-migration.js
+++ data/migrations/20211007181426_first-migration.js
@@ -1,26 +1,53 @@
 
 exports.up = async function (knex) {
-    await knex.schema.createTable('table_name', table => {
-
+    await knex.schema.createTable('recipies', table => {
+        table.increments('recipe_id')
+        table.string('recipe_name', 128).notNullable().unique()
+        table.timestamp('created_at').defaultTo(knex.fn.now())
     })
 
-    await knex.schema.createTable('table_name', table => {
-
+    await knex.schema.createTable('steps', table => {
+        table.increments('step_id')
+        table.integer('step_number').notNullable()
+        table.string('step_instructions').notNullable()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipe_id')
+            .onUpdate('RESTRICT')
+            .onDelete('RESTRICT')
     })
 
-    await knex.schema.createTable('table_name', table => {
-
+    await knex.schema.createTable('ingredients', table => {
+        table.increments('ingredient_id')
+        table.string('ingredient_name').notNullable()
     })
 
-    await knex.schema.createTable('table_name', table => {
-
+    await knex.schema.createTable('steps_ingredients', table => {
+        table.increments('steps_ingredients_id')
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT')
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT') 
+        table.decimal('quantity').notNullable()
     })
 };
 
 exports.down = async function (knex) {
     await knex.schema
-        .dropTableIfExists('')
-        .dropTableIfExists('')
-        .dropTableIfExists('')
-        .dropTableIfExists('')
+        .dropTableIfExists('steps_ingredients')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('recipies')
 };
--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,20 @@
 module.exports = {
     development: {
-      client: 'sqlite3',
-      useNullAsDefault: true, // needed for sqlite
-      connection: {
-        filename: './data/zoos.db3',
-      },
+      client: "sqlite3",
+      useNullAsDefault: true,
       migrations: {
-        directory: './data/migrations'
+        directory: "./data/migrations",
       },
       seeds: {
-        directory: './data/seeds'
+        directory: "./data/seeds",
       },
       pool: {
         afterCreate: (conn, done) => {
-          // runs after a connection is made to the sqlite engine
-          conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+          conn.run("PRAGMA foreign_keys = ON", done);
         },
       },
+      connection: {
+        filename: "./data/recipes.db3",
+      },
     },
-  };
+};
