2f47206b799e9f2302d54dbdd0312c6ecc5a692c
courtneyharris <harriscourtney98@gmail.com>
2021-10-07 23:26:45 -0400

    Working on seeds file

---
+++ data/seeds/02-recipes-holder.js
@@ -0,0 +1,4 @@
+
+exports.seed = function (knex) {
+   
+}
--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ const sharedConfig = {
     client: 'sqlite3',
     useNullAsDefault: true,
     migrations: { directory: './data/migrations' },
-    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done), }
-
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done), },
+    seeds: { directory: './data/seeds'}
 }
 
 module.exports= {
