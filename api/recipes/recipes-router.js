a83c58ba0069ce89d3f38c8801e0765964a8ca74
courtneyharris <harriscourtney98@gmail.com>
2021-10-07 20:56:24 -0400

    Working on router

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,5 +1,16 @@
 const router = require('express').Router()
 
+router.use('*', (req, res, next) => {
+    res.json({ api : 'up' })
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong inside the router',
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 
 module.exports = router
