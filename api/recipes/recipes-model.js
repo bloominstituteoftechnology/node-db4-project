c6367816d6cca9e3dce897a3116ba30e143836eb
Klove Adams <kloveadams@gmail.com>
2021-10-07 22:52:04 -0400

    it seems to work

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,10 +1,12 @@
 const db = require("../../data/db-config");
 
-async function getById(recipe_id){
+async function getRecipeById(recipe_id) {
   const recipe = await db("recipes as r")
     .join("steps as s", "r.recipe_id", "s.recipe_id")
+    .join("ingredients as i", "i.ingredient_id", "si.ingredient_id")
+    .join("step_ingredients as si", "si.step_id", "s.step_id")
     .where("r.recipe_id", recipe_id)
-    .select("r.recipe_name", "s.*")
+    .select("r.recipe_id", "r.recipe_name", "s.*", "i.*", "si.*")
     .orderBy("s.step_number");
 
   const result = {
@@ -19,11 +21,52 @@ async function getById(recipe_id){
         step_id: row.step_id,
         step_number: row.step_number,
         step_instructions: row.step_instructions,
+        ingredients: [
+          {
+            ingredient_name: row.ingredient_name,
+            ingredient_unit: row.ingredient_unit,
+            amount: row.amount,
+          },
+        ],
       });
     }
-  })
+  });
+
+  // recipe.reduce((acc, curr) => {
+  //   const {
+  //     recipe_id,
+  //     recipe_name,
+  //     step_id,
+  //     step_number,
+  //     instructions,
+  //     ingredient_id,
+  //     ingredient_name,
+  //     ingredient_unit,
+  //   } = curr;
+  //   if (acc.recipe_id) {
+  //   } else {
+  //     acc = {
+  //       recipe_id,
+  //       recipe_name,
+  //       steps: [
+  //         {
+  //           step_id,
+  //           step_number,
+  //           instructions,
+  //           ingredients: [
+  //             {
+  //               ingredient_id,
+  //               ingredient_name,
+  //               ingredient_unit,
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     };
+  //   }
+  // }, {});
 
   return result;
 }
 
-module.exports = { getById };
+module.exports = { getRecipeById };
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Recipe = require("./recipes-model");
 
 router.get("/:recipe_id", (req, res, next) => {
-  Recipe.getById(req.params.recipe_id)
+  Recipe.getRecipeById(req.params.recipe_id)
     .then((recipe) => {
       res.status(200).json(recipe);
     })
