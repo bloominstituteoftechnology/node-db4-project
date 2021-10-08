141e2f65b0a582878c36c587174c0f7bfe45b099
courtneyharris <harriscourtney98@gmail.com>
2021-10-07 23:13:31 -0400

    Migrations done, on to seeds

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const sharedConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done), }
+
+}
+
+module.exports= {
+    development: {
+        ...sharedConfig,
+        connection: { filename: './data/how_to.db3' },
+    },
+
+    testing: {
+        ...sharedConfig,
+        connection: { filename: './data/how_to.test.db3'}
+    },
+    production: {}
+}
