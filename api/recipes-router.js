a369825bdee2921ecf87afb7bf2ba79dab14bd8f
unknown <mykael.barnes@hotmail.com>
2021-10-21 17:41:38 -0700

    Mykael-Barnes

--- api/recipes-router.js
+++ api/recipes-router.js
@@ -5,7 +5,14 @@ const mw = require('./recipes-middleware')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-
+    const {id} = req.params
+    recipe.findById(id)
+        .then(recipe => {
+            res.status(200).json(recipe)
+        })
+        .catch(err => {
+            res.status(400).json({ message: err.message })
+        })
 })
 
 module.exports = router
