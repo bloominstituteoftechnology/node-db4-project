d2acf83f38f82a64944a31c9d6935013638786c7
priscila monteiro <pririmonteiro@me.com>
2021-11-02 21:52:53 -0700

    table created

--- data/migrations/create_recipes_book_table.js
+++ data/migrations/create_recipes_book_table.js
@@ -1,37 +1,46 @@
 exports.up = async function (knex) {
   await knex.schema
     .createTable('recipes', table => {
-      table.increments()
+      table.increments('recipe_id')
+      table.string('recipe_name', 256).notNullable().unique()
     })
     .createTable('ingredients', table => {
-      table.increments()
+      table.increments('ingredient_id')
+      table.string('ingredient_name', 256).notNullable().unique()
+      table.string('ingredient_unit', 80)
     })
     .createTable('steps', table => {
-      table.increments()
+      table.increments('step_id')
+      table.string('step_instructions', 256).notNullable()
+      table.integer('step_number').notNullable()
+      table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     })
     .createTable('steps_ingredients', table => {
-      table.increments()
+      table.increments('step_ingredient_id')
+      table.float('quantity').notNullable()
+      table.integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+      table.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     }) 
 };
   
-  // {
-  //   cars.increments();
-
-  //   cars.string('vin').notNullable().unique();
-  //   cars.string('make').notNullable();
-  //   cars.string('model').notNullable();
-  //   cars.decimal('mileage').unsigned().notNullable();
-  //   // unsigned ensures that the integer cannot be negative
-  //   // cars.integer('mileage').notNullable();
-  //   cars.string('title').notNullable();
-  //   // cars.string('title').notNullable().defaultTo('clean');
-  //   cars.string('transmission').notNullable();
-    
-
-  //   cars.timestamps(true, true);
-  // });
-
-
 exports.down = async function (knex) {
   await knex.schema
     .dropTableIfExists('steps_ingredients')
@@ -39,3 +48,4 @@ exports.down = async function (knex) {
     .dropTableIfExists('ingredients')
     .dropTableIfExists('recipes')
 };
+
