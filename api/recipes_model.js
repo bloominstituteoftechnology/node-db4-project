14c923413d1b15c79d1a3c57d500d2050d44c558
Nathan <nathan.forman@gmail.com>
2021-11-04 16:32:57 -0700

    Got data close to correct shape

--- api/recipes_model.js
+++ api/recipes_model.js
@@ -12,7 +12,28 @@ async function getRecipeById(recipe_id) {
         .leftJoin('steps as s', 's.recipe_id', 'r.recipe_id')
         .leftJoin('ingredients as i', 's.step_id', 'i.step_id')
         .leftJoin('quantities as q', 'i.ingredient_id', 'q.ingredient_id')
-    return rows
+
+    const result = {
+        recipe_id: rows[0].recipe_id,
+        recipe_name: rows[0].recipe_name,
+        steps: []
+    }
+
+    rows.forEach(row => {
+        if (row.step_number) {
+            result.steps.push({
+                step_id: row.step_id,
+                step_number: row.step_number,
+                step_instructions: row.step_instructions,
+                ingredient_name: row.ingredient_name,
+                quantity_amount: row.quantity_amount
+            })
+        } else {
+            result.steps = []
+        }
+    })
+
+    return result;
 }
 
 module.exports = {
