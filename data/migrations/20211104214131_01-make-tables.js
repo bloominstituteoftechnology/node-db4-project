5f9594e6722ac09ca690cbf61a3f9bb4ad82be8e
KRQuinn <kr.quinn82@gmail.com>
2021-11-04 15:03:59 -0700

    started setting up first migration. successfully creates required tables.

--- data/migrations/20211104214131_01-make-tables.js
+++ data/migrations/20211104214131_01-make-tables.js
@@ -1,8 +1,25 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', table => {
+        table.increments('recipe_id')
+    })
+    .createTable('recipe_steps', table => {
+        table.increments('step_id')
+    })
+    .createTable('step_ingredients', table => {
+        table.increments('ingredient_id')
+    })
+    .createTable('ingredients_quantity', table => {
+        table.increments('quantity_id')
+    })
+    
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema
+        .dropTableIfExists('ingredients_quantity')
+        .dropTableIfExists('step_ingredients')
+        .dropTableIfExists('recipe_steps')
+        .dropTableIfExists('recipes')
 };
