cf6d05f224e9f61e33617038130f4618aecb2982
Kat <kyevsukov@gmail.com>
2021-11-04 19:28:53 -0400

    finished ingredients seed

---
+++ data/seeds/02-ingredients.js
@@ -0,0 +1,11 @@
+exports.seed = function(knex, Promise) {
+  return knex('recipes').insert([   
+    { ingredient_name: 'olive oil', measurement_unit: "TBSP"},
+    { ingredient_name: 'egg(s)'},
+    { ingredient_name: 'Bread', measurement_unit: "Slice(s)"},
+    { ingredient_name: 'Pasta', measurement_unit: "Box"},
+    { ingredient_name: 'Water', measurement_unit: "Ounces"},
+    { ingredient_name: 'Marinara Sauce', measurement_unit: "Cups"},
+  ]);
+};
+
