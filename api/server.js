948e0c06f63bb13a91b100a525af4058be5bd578
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-18 16:53:35 -0800

    router is connected to server, data access fuctions completed in model.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const router = require('./router')
 
 const server = express()
 
 server.use(express.json())
+server.use('/api', router)
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        status: 500,
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = server
