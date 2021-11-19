6bc1b4b6e4a67c20759ecc177eef99fad5133e02
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-11-19 06:13:50 -0800

    wip

--- data/migrations/20211119044156_create-recipes-table.js
+++ data/migrations/20211119044156_create-recipes-table.js
@@ -1,8 +1,24 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    await knex.schema
+        .createTable('recipes', table => {
+            table.increments()
+        })
+        .createTable('ingredients', table => {
+            table.increments()
+        })
+        .createTable('steps', table => {
+            table.increments()
+        })
+        .createTable('step_ingredients', table => {
+            table.increments()
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    await knex.schema
+        .dropTableIfExists('step_ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes')
 };
--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ const sharedConfig = {
   },
   pool: {
     afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = IN', done)
+      conn.run('PRAGMA foreign_keys = ON', done)
     },
   },
 }
@@ -21,8 +21,13 @@ module.exports = {
   development: {
     ...sharedConfig,
     connection: {
-      filename: './recipes.sqlite3'
-    }
+      filename: './data/cook_book.db3'
+    },
   },
+  testing: {
+    ...sharedConfig, 
+    connection: { filename: './data/cook_book.test.db3'},
+  },
+  production: {}
 
 };
