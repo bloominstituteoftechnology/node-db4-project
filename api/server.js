7fa96d6a982020419d0dee48d95837cbc218b243
Raj-04 <1204patel.raj@gmail.com>
2021-12-09 18:15:23 -0500

    set up the error handling and the server

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const recipesRouter = require('./recipes/recipes_router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/recipes', recipesRouter)
+
+server.use('*', (req, res, next) => {
+  next({
+    status: 404,
+    message: 'The information you are trying to reach does not exist'
+  })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
+})
+
+module.exports = server
