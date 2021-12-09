2cb45dd7719c111534173922bf7738cd08c175a0
Ryan <snorekr@gmail.com>
2021-12-09 18:11:28 -0500

    get ingredients by recipe id

--- api/model.js
+++ api/model.js
@@ -2,6 +2,24 @@ const db = require('../data/db.config');
 
 const getRecipes = () => db('recipes');
 
-module.exports = {
-    getRecipes
+const getIngredientsByRecipeId = (recipe_id) => {
+// SELECT * 
+// FROM ingredients as i
+// LEFT JOIN recipe_book as rb
+//     ON rb.ingredient_id = i.ingredient_id
+// WHERE rb.recipe_id = {recipe_id};
+    return db('ingredients as i')
+        .leftJoin('recipe_book as rb', {
+            'rb.ingredient_id':'i.ingredient_id'
+        })
+        .where('rb.recipe_id', recipe_id)
 }
+
+const getRecipeById = async (recipe_id) => {
+
+};
+
+module.exports = {
+    getRecipes,
+    getIngredientsByRecipeId
+};
--- api/router.js
+++ api/router.js
@@ -5,8 +5,15 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {
         const recipes = await RecipeBook.getRecipes();
-        res.json(recipes)
+        res.json(recipes);
     } catch(e) { next(e) }
 });
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const ingredients = await RecipeBook.getIngredientsByRecipeId(req.params.id);
+        res.json(ingredients);
+    } catch(e) { next(e) }
+})
 
 module.exports = router;
