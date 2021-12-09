c796a99a33ee42a87d04c9777947e9ab268ab0e8
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-12-09 14:30:49 -0800

    added router and server files and content

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,14 @@
+const router= require('express').Router()
+
+router.use(`*`,(req, res, next)=>{
+    res.json({api: 'up'})
+})
+
+router.use((err, req, res, next)=>{
+    res.status(500).json({
+        customMessage:'something went wrong inside the recipes router',
+        message:err.message
+    })
+})
+
+module.exports=router
