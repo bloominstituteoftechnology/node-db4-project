caa3fe97b050721257fe441b96c340770c4f91bd
Raj-04 <1204patel.raj@gmail.com>
2021-12-09 18:32:23 -0500

    recipes table information(seed)

---
+++ data/seeds/01_recipes.js
@@ -0,0 +1,7 @@
+exports.seed = async function (knex) {
+  return await knex('recipes').insert([
+    { recipe_name: 'Oatmeal' },
+    { recipe_name: 'Sandwich' },
+    { recipe_name: 'Cinnamon-sugar toast'},
+  ])
+}
