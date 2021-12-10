2f49136cca6899edca237884a8a7797d4028c421
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-12-09 19:51:46 -0600

    done

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,9 @@
-function getRecipeById(recipe_id) =>{
+const db= require('../../data/db-config')
 
+ async function getRecipeById(recipe_id) {
+const recipesRows = await db('recipes as r')
+.where('recipe_id', recipe_id)
+return recipesRows
 }
 module.exports ={
     getRecipeById
