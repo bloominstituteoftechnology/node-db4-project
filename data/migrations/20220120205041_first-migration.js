a1a6ae13a26ad3ec7a458085e748a5fbe18b77c1
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-20 15:14:53 -0600

    Completed migrate file

--- data/migrations/20220120205041_first-migration.js
+++ data/migrations/20220120205041_first-migration.js
@@ -1,15 +1,52 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("recipes", (table) => {
+      table.increments("recipe_id");
+      table.text("recipe_name").notNullable().unique();
+    })
+    .createTable("ingredients", (table) => {
+      table.increments("ingredient_id");
+      table.text("ingredient_name").notNullable().unique();
+    })
+    .createTable("steps", (table) => {
+      table.increments("step_id");
+      table.text("step_instructions").notNullable();
+      table.integer("step_number");
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("steps_ingredients", (table) => {
+      table.increments("step_ingredient_id");
+      table
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table.integer("quantity").notNullable();
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists('steps_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
 };
