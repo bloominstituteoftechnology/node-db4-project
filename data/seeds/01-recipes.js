f2c4a1e49c8ac3094e57482fd19970eb66fb3be6
Lia Moua <moua0061@yahoo.com>
2022-01-20 21:34:47 -0500

    completed recipe seed

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,12 @@
+const recipes = [
+  {recipe_name: 'Broccoli Pesto Pasta'},
+  {recipe_name: 'Lemon Chicken'},
+  {recipe_name: 'Salmon en Papillote'},
+]
+
+exports.recipes = recipes 
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex('recipes').insert(recipes)
+}
+
