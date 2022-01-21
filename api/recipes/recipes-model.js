86e186b6b3fe20cfe8f483f4beab3ff3149b7831
Oscar Hernandez <oscarh425@outlook.com>
2022-01-20 18:01:19 -0800

    added model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,9 @@
-function getRecipeById(recipe_id){
-    
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id){
+    const recipeRows = await db('recipes')
+        .where('recipe_id', recipe_id)
+    return recipeRows
 }
 
 module.exports = {getRecipeById}
