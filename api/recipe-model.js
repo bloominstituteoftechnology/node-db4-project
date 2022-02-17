1948f9195e95dcefda4eb1a0d48df3cdcd8a7127
Kim Nguyen <kimnivore@gmail.com>
2022-02-17 12:47:47 -0800

    completed middleware

--- api/recipe-model.js
+++ api/recipe-model.js
@@ -1,9 +1,15 @@
 const db = require('../data/db-config');
 
+function getRecipes() {
+    return db('recipes');
+}
+
 async function getRecipeById(recipe_id) {
+    return db('recipes');
     // const result = await db()
 }
 
 module.exports = {
+    getRecipes,
     getRecipeById
 }
--- api/recipe-router.js
+++ api/recipe-router.js
@@ -1,2 +1,16 @@
-const Recipe = require('./recipe-model');
-const router = require('express').Router();
+const express = require('express');
+const Recipes = require('./recipe-model');
+// const {} = require('./recipe-middleware')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Recipes.getRec
+        .then(recipes => {
+            res.status(200).json(recipes);
+        })
+        .catch(next);
+})
+
+
+module.exports = router;
