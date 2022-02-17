a08983783f7b3ad453ea801fbacdcea3f87f5eb6
Peter Conley <peterdavidconley@gmail.com>
2022-02-17 16:23:00 -0500

    wrote db-config file

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@ const sharedConfig = {
       client: 'sqlite3',
       useNullAsDefault: true,
       migrations: { directory: './data/migrations'},
+      seeds: { directory: './data/seeds'},
       pool: {
         afterCreate: (conn, done) => {
           conn.run('PRAGMA foreign_keys = ON', done)
@@ -9,3 +10,17 @@ const sharedConfig = {
     }
   };
   
+  module.exports = {
+
+      development: {
+          ...sharedConfig,
+          connection: { filename: './data/cook_book.db3'},
+      },
+      testing: {
+          ...sharedConfig,
+          connection: { filename: './data/cook_book.test.db3'},
+      },
+      production: {
+
+      }
+  }
