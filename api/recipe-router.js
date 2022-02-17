c5653af1e222239e7d66ca63f62abcc3e6fb9a88
Kim Nguyen <kimnivore@gmail.com>
2022-02-17 12:50:42 -0800

    complete get routers

--- api/recipe-router.js
+++ api/recipe-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Recipes = require('./recipe-model');
-// const {} = require('./recipe-middleware')
+const { checkRecipeId } = require('./recipe-middleware')
 
 const router = express.Router();
 
@@ -10,6 +10,16 @@ router.get('/', (req, res, next) => {
             res.status(200).json(recipes);
         })
         .catch(next);
+});
+
+router.get('/:recipe_id', checkRecipeId, (req, res, next) => {
+    const { recipe_id } = req.params;
+
+    Recipes.getRecipeById(recipe_id)
+        .then(recipe => {
+            res.json(recipe)
+        })
+        .catch(next)
 })
 
 
