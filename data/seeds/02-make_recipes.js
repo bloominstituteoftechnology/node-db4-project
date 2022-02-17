fe8918e80ce13a7b4c4f5fab97cb1d65fc91956c
Peter Conley <peterdavidconley@gmail.com>
2022-02-17 18:29:44 -0500

    wrote export files

--- data/seeds/02-make_recipes.js
+++ data/seeds/02-make_recipes.js
@@ -58,6 +58,11 @@ const steps = [
 ]
 
 
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
+
+    await knex('recipes').insert(recipes)
+    await knex('ingredients').insert(ingredients)
+    await knex('steps').insert(steps)
+    await knex('step_ingredients').insert(step_ingredients)
 
 }
