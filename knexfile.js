73d020796ad8fafa9b7ec4ae9b698252f69d35c4
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-17 18:26:57 -0500

    setup

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+pool: {
+  afterCreate: (conn, done) => {
+    conn.run( 'PRAGMA foreign_keys = ON', done)
+  }
+}
+
+};
