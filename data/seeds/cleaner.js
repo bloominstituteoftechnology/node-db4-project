0c9f2143549a37478f5792ad7629df3b9bb328fd
Markeisha <markeisha94@gmail.com>
2022-02-17 11:27:58 -0600

    cleaner

--- data/seeds/cleaner.js
+++ data/seeds/cleaner.js
@@ -2,12 +2,10 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+const cleaner = require('knex-cleaner');
+
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+    return cleaner.clean(knex, {
+      ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
+    });
 };
