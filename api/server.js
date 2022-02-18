8444081b85f6367ed8f486e609ec44568097bdba
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-18 10:29:36 -0500

    more scaffolding

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
-const router = require('./router');
+const router = require('./recipes/recipes-router');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+
+
 server.use('/api/recipes', router);
+
+server.use('*', (req, res, next) => {
+  res.json({ api: 'up '})
+})
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
