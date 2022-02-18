54d1b968143374ad58136de6c45e1713e6314bea
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-17 22:49:29 -0600

    recipes-model has a new function added and catch all added into server.js

--- api/server.js
+++ api/server.js
@@ -8,4 +8,18 @@ server.use(express.json())
 
 server.use('/api/recipes', recipesRouter)
 
+server.use('*', (req, res, next) => {
+    next({
+      status: 404,
+      message: 'The information you are trying to reach does not exist'
+    })
+  })
+  
+  server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+    })
+  })
+  
+
 module.exports = server
