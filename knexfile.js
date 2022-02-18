d1aecfcb01dc15c8f271e2e62020d6156c48b012
marcel <marcel_rodrgez@yahoo.com>
2022-02-18 01:23:14 -0500

    made db config and linked it to knex

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+const sharedConfig = {
+    client: 'sqlite3',
+    migrations: {directory: './data/migrations'},
+    //sqlite3 new
+    useNullAsDefault: true,
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }
+}
+
+module.exports = {
+    development: {
+        ...sharedConfig,
+        connection: {filename: './data/cook_book.db3'},
+        seeds: {directory: './data/seeds'},
+
+    },
+    testing: {
+        ...sharedConfig,
+        connection: { filename: './data/cook_book.test.db3'},
+    },
+    production: {}
+}
