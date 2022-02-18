cd3708f27a867d78cfd8b267110532e364a7aff1
David Conley <dconley1989@gmail.com>
2022-02-17 22:05:16 -0700

    finished the models and the routing files

--- api/recipes-router.js
+++ api/recipes-router.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const recipes = require("./recipes-model");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const { recipe_id } = req.params;
+  const recipe = await recipes.getById(recipe_id);
+  return recipe;
+});
 
 module.exports = router;
