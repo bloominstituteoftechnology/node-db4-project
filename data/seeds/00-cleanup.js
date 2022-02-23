6c7658df98179101c21e84272d7ebeaf7a760bed
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-23 00:14:53 -0600

    WIP

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,8 +1,10 @@
-const cleaner = require('knex-cleaner');
+const { clean } = require('knex-cleaner');
 
-exports.seed = function(knex) {
-  return cleaner.clean(knex, {
-    mode: 'truncate', // resets ids
-    ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
-  });
+exports.seed = function (knex)
+{
+    return clean(knex, {
+        mode: 'truncate',
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+    });
 };
+
