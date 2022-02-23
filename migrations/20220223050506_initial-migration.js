91873cf5523a09cc1162b0237076818d5e5c7710
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-22 23:55:04 -0600

    WIP

--- migrations/20220223050506_initial-migration.js
+++ migrations/20220223050506_initial-migration.js
@@ -15,7 +15,16 @@ exports.up = async function (knex)
         })
         .createTable('steps', table =>
         {
-            table.increments();
+            table.increments('steps_id');
+            table.string('ingredient_name', 200).notNullable();
+            table.integer('step_number').notNullable();
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipe')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT');
         })
         .createTable('step_ingredients', table =>
         {
