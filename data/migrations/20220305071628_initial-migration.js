238d2bd049ae0793ac11549aa1ab617235e10e17
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-04 23:45:23 -0800

    created ingredients and recipes table

--- data/migrations/20220305071628_initial-migration.js
+++ data/migrations/20220305071628_initial-migration.js
@@ -2,14 +2,20 @@
 exports.up = async function(knex) {
   await knex.schema
   .createTable('recipes', table => {
-      table.increments()
+      table.increments('recipe_id')
+      table.string('recipe_name', 200).notNullable().unique()
   })
+
   .createTable('ingredients', table => {
-    table.increments()
+    table.increments('ingredient_id')
+    table.string('ingredient_name', 200).notNullable().unique()
+    table.string('ingredient_unit', 50)
   })
+
   .createTable('steps', table => {
     table.increments()
   })
+  
   .createTable('step_ingredients', table => {
     table.increments()
   })
