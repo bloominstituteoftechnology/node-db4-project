bb8f1afe16ff347079eb533520d363ecc64ad09f
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-04 23:57:31 -0800

    added steps table

--- data/migrations/20220305071628_initial-migration.js
+++ data/migrations/20220305071628_initial-migration.js
@@ -13,9 +13,18 @@ exports.up = async function(knex) {
   })
 
   .createTable('steps', table => {
-    table.increments()
+    table.increments('step_id')
+    table.string('step_text', 200).notNullable()
+    table.integer('step_number').notNullable()
+    table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
   })
-  
+
   .createTable('step_ingredients', table => {
     table.increments()
   })
