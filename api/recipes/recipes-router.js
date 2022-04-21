1a949cd86761b5c318371046adcc95b63dc98ee4
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-21 14:53:48 -0700

    done

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,15 +1,20 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const Recipe = require('./recipes-model');
 
-router.use('*', (req, res, next) => {
-    res.json({ api: 'up' })
-}) 
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            res.status(200).json(resource);
+        })
+        .catch(next);
+})
 
 router.use((err, req, res, next) => {
     res.status(500).json({
-        customMessage: 'something went wrong inside the recipes router',
+        customMessage: 'something went wrong inside the recipes-router',
         message: err.message,
-        stack: err.stack,
+        stack: err.stack
     })
 })
 
-module.exports = router
+module.exports = router;
