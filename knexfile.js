2e65d654441c9428b9749c187d3fba5d5e6eefe1
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-22 00:25:39 -0500

    seeded my data

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+const sharedConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: {directory: './data/migrations'},
+    seeds: {directory: './data/seeds'},
+    pool: {afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }
+}
+
+module.exports = {
+    development: {
+        ...sharedConfig,
+        connection: { filename: './data/cook_book.db3'},
+    }
+}
