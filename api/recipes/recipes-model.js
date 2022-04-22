0900ff90f45b0459e3400dd277e5362d3d0cd52e
GooieJS <conradklek@gmail.com>
2022-04-21 23:50:26 -0500

    submit

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,9 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`${recipe_id}`);
+const db = require('../../data/db-config.js');
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+        .where('recipe_id', recipe_id)
+    return recipeRows;
 }
 
 module.exports = { getRecipeById };
---
+++ seeds/02-make-recipes.js
@@ -0,0 +1,43 @@
+const recipes = [
+    { recipe_name: 'Broccoli Pesto Pasta' },
+    { recipe_name: 'Lemon Chicken' },
+    { recipe_name: 'Salmon en Papillote' },
+]
+
+const ingredients = [
+    { ingredient_name: 'Broccoli', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pesto', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pasta', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Lemon', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Chicken', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Salmon', ingredient_unit: 'lbs' },
+]
+
+const step_ingredients = [
+    { step_id: 2, ingredient_id: 1, quantity: 1 },
+    { step_id: 3, ingredient_id: 2, quantity: 1.5 },
+    { step_id: 3, ingredient_id: 3, quantity: 2 },
+    { step_id: 5, ingredient_id: 4, quantity: 1 },
+    { step_id: 5, ingredient_id: 5, quantity: 0.4 },
+    { step_id: 7, ingredient_id: 6, quantity: 1 },
+]
+
+const steps = [
+    { step_text: 'Heat pan', step_number: 1, recipe_id: 1 },
+    { step_text: 'Add Broccoli', step_number: 2, recipe_id: 1 },
+    { step_text: 'Add Pesto mixed with pasta', step_number: 3, recipe_id: 1 },
+
+    { step_text: 'Heat Oven', step_number: 1, recipe_id: 2 },
+    { step_text: 'Put chicken and lemon in oven', step_number: 2, recipe_id: 2 },
+    { step_text: 'Cook for 20 minutes', step_number: 3, recipe_id: 2 },
+
+    { step_text: 'fish out salmon', step_number: 1, recipe_id: 3 },
+    { step_text: 'add salmon to pan', step_number: 2, recipe_id: 3 },
+]
+
+exports.seed = async function (knex) {
+    await knex('recipes').insert(recipes)
+    await knex('ingredients').insert(ingredients)
+    await knex('steps').insert(steps)
+    await knex('step_ingredients').insert(step_ingredients)
+}
