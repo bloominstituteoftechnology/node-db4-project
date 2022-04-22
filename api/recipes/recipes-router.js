6f58bf8d247b9c5c4b6ee64c6e9299c76ca0d7f5
CameronRuch <cameron.ruch@yahoo.com>
2022-04-21 23:00:53 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
 
-router.use('*', (req, res) => {
-    res.json({ api: 'up' })
+
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(next)
 })
 
-router.use((err, req, res, next) => {
-    res.status(500).json({
-        customMessage: 'something went wrong in router',
-        message: err.message,
-        stack: err.stack
-    })
-})
+
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -8,4 +8,16 @@ server.use(express.json())
 
 server.use('/api/recipes', recipesRouter)
 
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: 'something went wrong in router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 module.exports = server;
