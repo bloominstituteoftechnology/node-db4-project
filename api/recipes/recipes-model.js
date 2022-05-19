8cecebb2afcbd90dccca9985748f9779edcd1b5b
unknown <dshipuk@yahoo.com>
2022-05-19 15:45:06 -0700

    Completed

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,9 @@
+const db = require("../../data/db-config")
 
-
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`Awesome ${recipe_id}`)
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db("recipes as r")
+        .where("recipe_id", recipe_id)
+    return recipeRows
 }
 
 
