fe0b40c9ec69d9855a2ae2e4bd93c1abdb62daaf
unknown <dshipuk@yahoo.com>
2022-05-19 14:41:56 -0700

    wip

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,11 @@
+
+
+function getRecipeById(recipe_id) {
+    return Promise.resolve(`Awesome ${recipe_id}`)
+}
+
+
+
+module.exports = {
+    getRecipeById
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
-const server = require("./api/server");
+const server = require("./api/server.js");
 
 const port = process.env.PORT
 
