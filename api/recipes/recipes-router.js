b7a4302596dc0489992d66eb8382f3e929096322
unknown <dshipuk@yahoo.com>
2022-05-19 14:17:08 -0700

    Added Server created Routing with error handling

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router()
+
+
+router.use("*", (req, res) => {
+    res.json({
+        api: "up"
+    })
+})
+
+router.use( (err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: "Something went wrong inside the recipes router",
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+
+module.exports = router;
