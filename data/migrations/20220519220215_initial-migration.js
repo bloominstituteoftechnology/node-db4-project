bee2a3b44d078b752c51579ffd355a39866ec75e
Nathan Cai <wenjuncai97@gmail.com>
2022-05-19 18:09:29 -0400

    wip

--- data/migrations/20220519220215_initial-migration.js
+++ data/migrations/20220519220215_initial-migration.js
@@ -4,20 +4,23 @@
  */
 exports.up = async function(knex) {
   await knex.schema
-    .createTable('recipes', table => {
-        table.increments()
+    .createTable('recipes', tbl => {
+        tbl.increments('recipe_id')
+        tbl.string('recipe_name', 200).notNullable().unique()
     })
 
-    .createTable('ingredients', table => {
-        table.increments()
+    .createTable('ingredients', tbl => {
+        tbl.increments('ingredients')
+        table.string('ingredients_name', 200).notNullable().unique()
+        table.string('ingredients_unit', 50)
     })
 
-    .createTable('steps', table => {
-        table.increments()
+    .createTable('steps', tbl => {
+        tbl.increments()
     })
 
-    .createTable('step_ingredients', table => {
-        table.increments()
+    .createTable('step_ingredients', tbl => {
+        tbl.increments()
     })
 
 };
