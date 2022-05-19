f2c08c3c927e99f37c4633a387837ff958e74e27
Nathan Cai <wenjuncai97@gmail.com>
2022-05-19 18:19:53 -0400

    commit

--- data/migrations/20220519220215_initial-migration.js
+++ data/migrations/20220519220215_initial-migration.js
@@ -11,16 +11,40 @@ exports.up = async function(knex) {
 
     .createTable('ingredients', tbl => {
         tbl.increments('ingredients')
-        table.string('ingredients_name', 200).notNullable().unique()
-        table.string('ingredients_unit', 50)
+        tbl.string('ingredients_name', 200).notNullable().unique()
+        tbl.string('ingredients_unit', 50)
     })
 
     .createTable('steps', tbl => {
-        tbl.increments()
+        tbl.increments('step_id')
+        tbl.string('step_text', 200).notNullable()
+        tbl.integer('step_number').notNullable()
+        tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
 
     .createTable('step_ingredients', tbl => {
-        tbl.increments()
+        tbl.increments('step_ingredient_id')
+        tbl.float('quantity').notNullable()
+        tbl.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id')
+            .inTable('steps')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        tbl.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
 
 };
