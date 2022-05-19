5ec4c722c53a544323c798d0182f7a2a20253c9c
unknown <dshipuk@yahoo.com>
2022-05-19 15:07:14 -0700

    Created Recipe and Ingredient Tables

--- data/migrations/20220519215500_initial-migration.js
+++ data/migrations/20220519215500_initial-migration.js
@@ -4,19 +4,22 @@
  */
 exports.up = async function(knex) {
   await knex.schema
-    .createTable("recipes" table => {
+    .createTable("recipes", table => {
+        table.increments("recipe_id")
+        table.string("recipe_name", 200).notNullable().unique()
+    })
+
+    .createTable("ingredients", table => {
+        table.increments("ingredient_id")
+        table.string("ingredient_name", 200).notNullable().unique()
+        table.string("ingredient_unit", 50)
+    })
+
+    .createTable("steps", table => {
         table.increments()
     })
 
-    .createTable("ingredients" table => {
-        table.increments()
-    })
-
-    .createTable("steps" table => {
-        table.increments()
-    })
-
-    .createTable("steps_ingredients" table => {
+    .createTable("steps_ingredients", table => {
         table.increments()
     })
 
