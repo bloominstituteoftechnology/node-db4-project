3741b5b7f08986523b6d2be3c0bed810ccc62efa
Muniba Ahmed <mahmed93@berkeley.edu>
2022-07-21 14:31:04 -0700

    wip

--- data/migrations/20220721210413_initial-migration.js
+++ data/migrations/20220721210413_initial-migration.js
@@ -1,10 +1,13 @@
 exports.up = async function (knex) {
   await knex.schema
     .createTable("recipes", (table) => {
-      table.increments();
+      table.increments("recipe_id");
+      table.string("recipe_name", 200).notNullable().unique();
     })
     .createTable("ingredients", (table) => {
-      table.increments();
+      table.increments("ingredient_id");
+      table.string("ingredient_name", 200).notNullable().unique();
+      table.string("ingredient_unit", 50);
     })
     .createTable("steps", (table) => {
       table.increments();
