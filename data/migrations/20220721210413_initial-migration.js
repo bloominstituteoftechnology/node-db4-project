e319b73579c945095ad3dce3b936d94c0e193a48
Muniba Ahmed <mahmed93@berkeley.edu>
2022-07-21 14:40:12 -0700

    wip

--- data/migrations/20220721210413_initial-migration.js
+++ data/migrations/20220721210413_initial-migration.js
@@ -10,10 +10,29 @@ exports.up = async function (knex) {
       table.string("ingredient_unit", 50);
     })
     .createTable("steps", (table) => {
-      table.increments();
+      table.increments("step_id");
+      table.string("step_text", 200).notNullable();
+      table.integer("step_number").notNullable();
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
     .createTable("step_ingredients", (table) => {
-      table.increments();
+      table.increments("step_ingredient_id");
+      table.float("quantity").notNullable();
+      table
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
