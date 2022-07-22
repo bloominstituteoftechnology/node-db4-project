2fc3c1a490027574ae3044d0ad78fef8a275d1c6
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-22 16:39:47 -0600

    almost complete, working on model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,9 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe with ${recipe_id} kohlten`)
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+    .where('recipe_id', recipe_id)
+    return recipeRows
 }
 
 module.exports = { getRecipeById }
