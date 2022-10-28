a463430bb471dff385aaba9a7ab2f6813a83475f
unknown <saradawsone@gmail.com>
2022-10-28 13:19:55 -0600

    added deps

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './recipes.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+
+};
