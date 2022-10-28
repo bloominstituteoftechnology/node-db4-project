1c29d7928afcbc9e79a555001d8ca45128723b69
unknown <saradawsone@gmail.com>
2022-10-28 16:48:44 -0600

    added seed files

--- data/migrations/20221028202340_initial-migration.js
+++ data/migrations/20221028202340_initial-migration.js
@@ -5,7 +5,7 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('recipes', table => {
-        table.increments('recipes_id')
+        table.increments('recipe_id')
         table.string('recipe_name', 200).notNullable().unique()
     })    
     .createTable('ingredients', table => {
