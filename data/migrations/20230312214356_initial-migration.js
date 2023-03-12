5dc8917f8c43ece190a45ce4675bb440d075c9a6
Robby Lewis <Robbysim37@gmail.com>
2023-03-12 18:04:20 -0400

    wip

--- data/migrations/20230312214356_initial-migration.js
+++ data/migrations/20230312214356_initial-migration.js
@@ -5,16 +5,43 @@
 exports.up = async function(knex) {
    await knex.schema
     .createTable(`recipes`, table => {
-        table.increments()
+        table.increments(`recipe_id`)
+        table.string(`recipe_name`, 200).notNullable().unique()
     })
     .createTable(`ingredients`, table => {
-        table.increments()
+        table.increments(`ingredient_id`)
+        table.string(`ingredient_name`, 200).notNullable().unique()
+        table.string(`ingredient_unit`, 50)
     })
     .createTable(`steps`, table => {
-        table.increments()
+        table.increments(`step_id`)
+        table.string(`step_text`,200).notNullable()
+        table.integer(`step_number`).notNullable()
+        table.integer(`recipe_id`)
+        .unsigned()
+        .notNullable()
+        .references(`recipe_id`)
+        .inTable(`recipes`)
+        .onDelete(`RESTRICT`)
+        .onUpdate(`RESTRICT`)
     })
-    .createTable(`step_ingrediants`, table => {
-        table.increments()
+    .createTable(`step_ingredients`, table => {
+        table.increments(`step_ingredient_id`)
+        table.float(`quantity`).notNullable()
+        table.integer(`step_id`)
+        .unsigned()
+        .notNullable()
+        .references(`step_id`)
+        .inTable(`steps`)
+        .onDelete(`RESTRICT`)
+        .onUpdate(`RESTRICT`)
+        table.integer(`ingredient_id`)
+        .unsigned()
+        .notNullable()
+        .references(`ingredient_id`)
+        .inTable(`ingredients`)
+        .onDelete(`RESTRICT`)
+        .onUpdate(`RESTRICT`)
     })
 };
 
@@ -24,7 +51,7 @@ exports.up = async function(knex) {
  */
 exports.down = async function(knex) {
   await knex.schema
-  .dropTableIfExists(`step_ingrediants`)
+  .dropTableIfExists(`step_ingredients`)
   .dropTableIfExists(`steps`)
   .dropTableIfExists(`ingredients`)
   .dropTableIfExists(`recipes`)
