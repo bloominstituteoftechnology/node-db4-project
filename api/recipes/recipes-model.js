dda2ab00ae7dc597f5a721b9c380e305e5f21398
Robby Lewis <Robbysim37@gmail.com>
2023-03-12 19:06:32 -0400

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,8 @@
-const getRecipeById = (recipe_id) => {
-    return Promise.resolve(`recipe ${recipe_id}`)
+const db = require(`../../data/db-config`)
+
+const getRecipeById = async (recipe_id) => {
+    const recipeRows = await db(`recipes as r`).where(`recipe_id`, recipe_id)
+    return recipeRows
 }
 
 module.exports = {
