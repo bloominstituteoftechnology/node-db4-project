441710c28814ae59b9e9cdff1042cae7ac908b36
BruvLio <lionel.mbayu@gmail.com>
2023-05-11 15:46:43 -0500

    wip

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./api/server");
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Listing on port ${PORT} lets get active`);
+  console.log(`Listening on port ${PORT} lets get active`);
 });
