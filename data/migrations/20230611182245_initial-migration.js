74f580d043aca53bd9eb94387fb014a9728cd618
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-06-11 14:52:54 -0400

    Completed initial Migrations

--- data/migrations/20230611182245_initial-migration.js
+++ data/migrations/20230611182245_initial-migration.js
@@ -2,33 +2,64 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up =  async function(knex) {
-  await knex.schema
-  .createTable('recipes', table => {
-    table.increments})
-    
-  .createTable('ingredients', table => {
-    table.increments})
-    
-  .createTable('steps', table => {
-    table.increments})
-    
-  .createTable('step_ingredients', table => {
-    table.increments})
-    
+exports.up = async function (knex) {
+    await knex.schema
+        .createTable('recipes', table => {
+            table.increments('recipe_id')
+            table.string('recipe_name', 200).notNullable().unique()
+        })
+
+        .createTable('ingredients', table => {
+            table.increments('ingredient_id')
+            table.string('ingredient_name', 200).notNullable().unique()
+            table.string('ingredient_units', 50)
+        })
+
+        .createTable('steps', table => {
+            table.increments('step_id')
+            table.string('step_name', 200).notNullable().unique()
+            table.integer('step_number').notNullable()
+            table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        })
+
+        .createTable('step_ingredients', table => {
+                table.increments('step_ingredients_id')
+                table.float('quanity').notNullable()
+                table.integer('step_id').notNullable()
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+                step_ingredients.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+        })
+
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async function(knex) {
+exports.down = async function (knex) {
     await knex.schema
-    .dropTableIfExists('step_ingredients')
-    .dropTableIfExists('steps')
-    .dropTableIfExists('ingredients')
-    .dropTableIfExists('recipes')
-    
-    
-  
+        .dropTableIfExists('step_ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes')
+
+
+
 };
