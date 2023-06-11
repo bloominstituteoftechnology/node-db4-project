65c3a0913016991184f204cb8a908ccba5787119
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-06-11 16:58:03 -0400

    WIP

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,22 +1,22 @@
 const router = require('express').Router()
-const Recipe = require('./recipers-model')
+const Recipe = require('./recipes-modelnpm ')
 
-router.get('/:reciper_id', (req, res, next) => {
-    Recipe.getRecipeById(req.params.reciper_id)
-        .then(resource => {
-            res.status(200).json(resource)
-        })
-        .catch(next)
-})
-
-
-
-router.use((err, req, res, next) => { //eslint-disable-line
-    res.status(500).json({
-        customMessage:'something went wrong inside the recipes router',
-        message: err.message,
-        stack: err.stack,
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+    .then(resource =>{
+        res.status(200).json(resource)
     })
+    .catch(next)
 })
+
+
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: "Something Wrong",
+        message: err.message
+    })
+}) 
 
 module.exports = router
