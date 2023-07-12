26d9eab1d0d1d8a4e99cc27acef57b51352b9c0d
Michael Newell <michael.newell212@gmail.com>
2023-07-12 14:42:49 -0600

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,4 @@ const knex = require('knex')
 const configurations = require('../knexfile.js')
 const environment = process.env.NODE_ENV || 'development'
 
-module.exports = knex(configurations(environment))
+module.exports = knex(configurations[environment])
