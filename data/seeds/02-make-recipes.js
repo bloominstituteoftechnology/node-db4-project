442ef8dbee90d96f217cd0618369cef8c3961720
Joshua Burnett <joshuaburnett429@gmail.com>
2023-07-19 12:57:44 -0400

    flying home

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -1,3 +1,19 @@
+const recipes = [
+    { recipe_name: 'Broccoli Pesto Pasta' },
+    { recipe_name: 'Lemon Chicken' },
+    { recipe_name: 'Salmon en Papillote' },
+]
+
+const ingredients = [
+    { ingredient_name : 'Broccoli', ingredient_unit: 'lbs' },
+    { ingredient_name : 'Pesto', ingredient_unit: 'lbs' },
+    { ingredient_name : 'Pasta', ingredient_unit: 'lbs' },
+    { ingredient_name : 'Lemon', ingredient_unit: 'slices' },
+    { ingredient_name : 'Chicken', ingredient_unit: 'kilos' },
+    { ingredient_name : 'Salmon', ingredient_unit: 'grams' },
+
+]
+
 exports.seed = function (knex) {
     
 }
