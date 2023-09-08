5fdb6fd50dbc60201968d95afb51f7ef824f9f5b
CancuiW <can.cui@wayne.edu>
2023-09-07 17:27:51 -0700

    changes

--- knexfile.js
+++ knexfile.js
@@ -3,28 +3,22 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+const sharedConfig={
+  client: 'sqlite3',
+  migrations: { directory: './data/migrations'},
+  seeds: {directory: './data/seeds'},
+  useNullAsDefault: true,
+  pool: { afterCreate: (conn, done) => { conn.run('PRAGMA foreign_keys = ON', done)}}
+}
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/recipe.db3'
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-   seeds: {
-      directory: './data/seeds'
-    },
-    useNullAsDefault:true,
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
-    },
+    ...sharedConfig,
+    connection: {filename: './data/recipe.db3'}  
   },
-
-  
-  
+  testing:{
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.test.db3' }  
+  }
 
 };
