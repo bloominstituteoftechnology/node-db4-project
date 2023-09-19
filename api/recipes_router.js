2012a3f7b77f2aa61f51bd1ab5407d6b7e28593e
Ryan Oberly <ryanjoberly@gmail.com>
2023-09-19 14:35:25 -0600

    final branch commit

--- api/recipes_router.js
+++ api/recipes_router.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const helpers = require('./recipes_model')
+const Recipe = require('./recipes_model')
 
 const router = express.Router();
 
 router.get('/recipes/:id', async (req, res, next) => {
     const { id } = req.params
   
-    helpers.getRecipeById(id)
+    Recipe.getRecipeById(id)
         .then(recipe => {
             res.status(200).json(recipe)
         })
