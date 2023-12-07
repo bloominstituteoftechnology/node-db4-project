0a9bac47c687c7661296cd366ee65837ae2962f4
Justin Byrd <justinbyrd7@gmail.com>
2023-12-07 09:28:35 -0600

    wip

--- data/migrations/20231206222834_create-tables.js
+++ data/migrations/20231206222834_create-tables.js
@@ -6,15 +6,45 @@ exports.up = async function (knex) {
   await knex.schema
     .createTable("recipes", (tbl) => {
       tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable().unique();
     })
     .createTable("ingredients", (tbl) => {
       tbl.increments("ing_id");
+      tbl.string("ing_name", 128).notNullable();
+      tbl.string("ing_unit", 128);
     })
     .createTable("steps", (tbl) => {
       tbl.increments("step_id");
+      tbl.string("step_instructions", 128);
+      tbl.integer("step_number").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("CASCADE") //or .onDelete('RESTRICT')?
+        .onUpdate("CASCADE"); //or .onUpdate('RESTRICT')?
     })
     .createTable("steps_ingredients", (tbl) => {
       tbl.increments("step_ing_id");
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("CASCADE") //or .onDelete('RESTRICT')?
+        .onUpdate("CASCADE"); //or .onUpdate('RESTRICT')?
+      tbl
+        .integer("ing_id")
+        .unsigned()
+        .notNullable()
+        .references("ing_id")
+        .inTable("ingredients")
+        .onDelete("CASCADE") //or .onDelete('RESTRICT')?
+        .onUpdate("CASCADE"); //or .onUpdate('RESTRICT')?
+      tbl.float("ing_quantity").notNullable();
     });
 };
 
@@ -23,5 +53,9 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
-  await knex.schema;
+  await knex.schema
+    .dropTableIfExists("steps_ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };
