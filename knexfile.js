ab8ab649f9dabe9f08b45865faa70a79c3d761e0
Justin Byrd <justinbyrd7@gmail.com>
2023-12-07 09:03:42 -0600

    wip

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,25 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const sharedConfig = {
+  client: "sqlite3",
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+  //sqlite specific
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+  },
+};
+
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3",
-    },
+    ...sharedConfig,
+    connection: { filename: "./data/cook_book.db3" },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: "./data/cook_book.test.db3" },
   },
 };
