161b057f3fb4273acc58cd065b8745e22b4d0193
Justin Byrd <justinbyrd7@gmail.com>
2023-12-07 14:58:56 -0600

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,16 @@
 const db = require("../../data/db-config");
 
 async function getRecipeById(recipe_id) {
-  const recipeRows = await db("recipes as r").where("recipe_id", recipe_id);
+  const recipeRows = await db("recipes as r")
+    .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
+    .select(
+      "r.recipe_id",
+      "r.recipe_name",
+      "s.step_id",
+      "s.step_number",
+      "s.step_instructions"
+    )
+    .where("r.recipe_id", recipe_id);
   return recipeRows;
 }
 
