52a791d738ef6d98762ef81b4036e0c46fa90095
Levi Bohn <levibohn@gmail.com>
2024-03-05 15:56:48 -0600

    wip

--- data/migrations/20240305214126_initial-migration.js
+++ data/migrations/20240305214126_initial-migration.js
@@ -5,13 +5,16 @@
 exports.up = async function (knex) {
     await knex.schema
         .createTable('recipes', table => {
-            table.increments()
+            table.increments('recipe_id')
+            table.string('recipe_name', 200).notNullable().unique()
         })
         .createTable('ingredients', table => {
             table.increments()
         })
         .createTable('steps', table => {
-            table.increments()
+            table.increments('ingredient_id')
+            table.string('ingredient_name', 200).notNullable().unique()
+            table.string('ingredient_unit', 50)
         })
         .createTable('step_ingredients', table => {
             table.increments()
