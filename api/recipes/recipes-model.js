05aad45b1e8a4a96d0d84bb056fe735afc0c4db1
Levi Bohn <levibohn@gmail.com>
2024-03-05 19:43:01 -0600

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,10 @@
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe with id ${recipe_id}`)
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+        .where('recipe_id', recipe_id)
+
+    return recipeRows
 }
 
 module.exports = { getRecipeById }
